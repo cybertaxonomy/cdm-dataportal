d5a5bb4422633564480de42f33fb5f7db7744498
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2007-12-11 16:35:39 +0000

    Copyright & License added to code

--- modules/taxon_experts/resources/geography-ed2_to_taxomomyXML.php
+++ modules/taxon_experts/resources/geography-ed2_to_taxomomyXML.php
@@ -22,8 +22,14 @@
  * 1. Download geography_ed2.zip from the URL noted above
  * 2. Edit the variable $zip_file
  * 3. Run the script from your browser.
+ * 
+ * Copyright (C) 2007 EDIT
+ * European Distributed Institute of Taxonomy 
+ * http://www.e-taxonomy.eu
+ * 
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * See LICENSE.TXT at the top of this package for the full license terms.
  */
-
 
 
 $zip_file = "geography_ed2.zip";
