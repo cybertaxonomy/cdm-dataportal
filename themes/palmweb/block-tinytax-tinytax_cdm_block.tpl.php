79c7280fee243bfe8989370f6f39f5e79f9d7e39
DavidTaylor <davidtaylor@localhost>
2008-03-20 18:59:00 +0000

    includes palmweb.profile with all admin settings

--- themes/palmweb/block-tinytax-tinytax_cdm_block.tpl.php
+++ themes/palmweb/block-tinytax-tinytax_cdm_block.tpl.php
@@ -1,9 +1,10 @@
   <div class="block block-<?php print $block->module; ?>" 
     id="block-<?php print $block->module; ?>-<?php print $block->delta; ?>">
    
-    <h6 class ="tinytaxroot">Govaerts and Dransfield 2007</h6>
-    <h2 class="searchtitle"><?php print $block->subject; 
+    <h6 class ="tinytaxroot"> <?php print $block->authors; 
+    theme()?> </h6>
     
+    <h2 class="searchtitle"><?php print $block->subject; 
     theme()?></h2>
     <div class="searchccontent"><?php print $block->content; ?></div>
     
