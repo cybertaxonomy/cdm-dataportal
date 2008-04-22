b9081d63af115354851f2a0424cb6c0a3b25c073
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2008-04-22 15:42:52 +0000

    about to remove function which marks selected items in the tree

--- modules/cdm_dataportal/cdm_taxontree/js/cdm_taxontree_widget.js
+++ modules/cdm_dataportal/cdm_taxontree/js/cdm_taxontree_widget.js
@@ -13,40 +13,55 @@
  /**
   * 
   */
-  $.fn.cdm_taxontree_widget = function() {
+  $.fn.cdm_taxontree_widget = function(options) {
+		
+		var opts = $.extend({},$.fn.cdm_taxontree_widget.defaults, options);
 		
 		return this.each(function() {
-		
-		  var widget = $(this);
-		  var listing = $(this).children('ul.listing');
 		  
-		  $(this).find('ul.cdm_taxontree li .widget_select').each(function() {		  
-				// is it in listing of selected?
-				var value = $(this).attr('title');
-			  if(listing.children('input .value_'+value)){
-			   $(this).addClass('selected');
-			  }
-		  });
+		  // init taxontree
+
+		  $(this).find('ul.cdm_taxontree').cdm_taxontree(true, options);
+
+		  
+		  var widget = $(this);
+		  var select = $('.cdm_taxontree_widget select');
+		  	  
+     // #### select.click(function(){
+     // ####   $(this).children().attr('selected', 'selected');
+     // #### });
+     		 
+		 // #### select.find('option').each(function() {		  
+		 // ####	widget.find('ul.cdm_taxontree li .value_'+$(this).text()).addClass('selected');
+		 // #### });
 		  
 			$(this).find('ul.cdm_taxontree li .widget_select').click(function(event) {
 				event.stopPropagation();
 			  var value = $(this).attr('title');
-			  if(listing.find('input .value_'+value).length > 0){
-			    // remove from listing
-			    $(this).removeClass('selected');
-			    listing.children('input .value_'+value).parent().remove();
-			   
+			  if(select.children('[value='+value+']').length > 0){
+			    // remove from select
+			    // #### widget.find('ul.cdm_taxontree li .value_'+value).removeClass('selected');
+			    select.children('[value='+value+']').remove();
         } else {
-          // add to listing
-         $(this).addClass('selected');
-         listing.children().append('<li><input type="text" value="'+value+'"/></li>')
+         // add to select
+         if(!opts.multiselect){
+           // remove all from select
+           // #### widget.find('ul.cdm_taxontree li .widget_select').removeClass('selected');
+           select.children().remove();
+         }
+         // #### widget.find('ul.cdm_taxontree li .value_'+value).addClass('selected');
+         select.append('<option value="'+value+'" selected="selected">'+value+'</option>')
         }
         
 			}); // END click()
 		}); // END each(
-	}; // END cdm_taxontree_widget()
+	}; // END cdm_taxontree_widget() 
 	 
 })(jQuery);
 
+$.fn.cdm_taxontree_widget.defaults = {  // set up default options
+	element_name:           'widgetval',  // 
+	multiselect:            false,        //
+};
 
-$(document).ready(function() {$('.cdm_taxontree_widget').cdm_taxontree_widget();});
+//$(document).ready(function() {$('.cdm_taxontree_widget').cdm_taxontree_widget();});
