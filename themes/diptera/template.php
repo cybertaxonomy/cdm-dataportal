deaab715a0eccb5fbf60ac6c4db8689e0c0c0735
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2009-05-19 15:34:54 +0000

    improvements for diptera

--- themes/diptera/template.php
+++ themes/diptera/template.php
@@ -69,14 +69,17 @@ function diptera_cdm_related_taxon($taxonSTO, $reltype_uuid = '', $displayNomRef
   $taxon_str  = theme('cdm_name', $taxonSTO->name, true, false, false, false, false);
   
   $out = '<span class="relation_sign">'.$relsign.'</span>'.$name_prefix.$taxon_str;
+  
   if($taxonSTO->name->taggedName){
     $authorsStr = cdm_taggedtext_value($taxonSTO->name->taggedName, "authors");
     $authorsHtml = '<span class="authors">'.$authorsStr.'</span>';
     if(isset($taxonSTO->name->nomenclaturalReference)){
       $authorsHtml = l($authorsHtml, "/cdm_dataportal/reference/".$taxonSTO->name->nomenclaturalReference->uuid, array(), NULL, NULL, FALSE, TRUE);  
     }
-
     $out .= ( str_beginsWith($authorsStr,'(') ? ' ' : ', ') . $authorsHtml;
+    if($taxonSTO->name->nomenclaturalReference->pages){
+      $out .= '<span class="pages">:&nbsp;' . $taxonSTO->name->nomenclaturalReference->pages . '</span>'; 
+    }
   }else{
     if($taxonSTO->name->nomenclaturalReference){
       $out .= ' '.theme('cdm_nomenclaturalReferenceSTO', $taxonSTO->name->nomenclaturalReference, TRUE, '', '<br />', 'li', TRUE);
@@ -86,5 +89,79 @@ function diptera_cdm_related_taxon($taxonSTO, $reltype_uuid = '', $displayNomRef
   return $out;
 
 }
+
+/**
+ * Allows theaming of the taxon page tabs
+ * 
+ * @param $tabname
+ * @return unknown_type
+ */
+function diptera_cdm_taxonpage_tab($tabname){
+  switch($tabname){
+    case 'Synonymy' : return t('Nomenclature'); break;
+    default : return t($tabname); 
+  }
+}
+
+function diptera_cdm_taxon_page_general($taxonTO, $page_part) {
+  
+  if(!$page_part){
+    $page_part = 'description';
+  }
+  $page_part = variable_get('cdm_dataportal_taxonpage_tabs', 1) ? $page_part : 'all';  
+
+  $out = '';
+  $out .= theme('cdm_back_to_search_result_button');
+  $out .= theme('cdm_acceptedFor');
+  
+  if($page_part == 'description' || $page_part == 'all'){
+    $out .= '<div id="general">';
+    $out .= theme('cdm_taxon_page_description', $taxonTO);
+    $out .= '</div>';
+  }
+  
+  if($page_part == 'images' || $page_part == 'all'){
+    $out .= '<div id="images">';
+    if($page_part == 'all'){
+      $out .= '<h2>'.t('Images').'</h2>';
+    }
+    $out .= theme('cdm_taxon_page_images', $taxonTO);
+    $out .= '</div>';
+  }
+
+  if($page_part == 'synonymy' || $page_part == 'all'){
+    $out .= '<div id="synonymy">';
+    if($page_part == 'all'){
+      $out .= '<h2>'.t('Synonymy').'</h2>';
+    }
+    if(!variable_get('cdm_dataportal_nomref_in_title', 1)){
+      //---
+       $taxon_str  = theme('cdm_name', $taxonTO->name, true, false, false, false, false);
+       
+       $out .= $taxon_str;
+       
+       $authorsStr = cdm_taggedtext_value($taxonTO->name->taggedName, "authors");
+       $authorsHtml = '<span class="authors">'.$authorsStr.'</span>';
+       
+       if(isset($taxonSTO->name->nomenclaturalReference)){
+          $authorsHtml = l($authorsHtml, "/cdm_dataportal/reference/".$taxonTO->name->nomenclaturalReference->uuid, array(), NULL, NULL, FALSE, TRUE);  
+       }
+       $out .= ( str_beginsWith($authorsStr,'(') ? ' ' : ', ') . $authorsHtml;
+       if($taxonTO->name->nomenclaturalReference->pages){
+          $out .= '<span class="pages">:&nbsp;' . $taxonTO->name->nomenclaturalReference->pages . '</span>'; 
+       } 
+      // ---
+    }
+    $out .= theme('cdm_taxon_page_synonymy', $taxonTO);
+
+    if(variable_get('cdm_dataportal_display_name_relations', 1)){
+      $out .= theme('cdm_nameRelations', $taxonTO->name->nameRelations);
+     }
+    $out .= '</div>';
+  }
+
+  return $out;
+}
+
 
 ?>
