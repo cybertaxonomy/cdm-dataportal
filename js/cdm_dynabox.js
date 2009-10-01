26acb3f3ffd98459bc264b92b8caf1c6f4203fce
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2009-10-01 09:27:56 +0000

    tagging as module-cdm_dataportal-RELEASE2.0 for production server [update]

---
+++ js/cdm_dynabox.js
@@ -0,0 +1,35 @@
+// $Id$
+
+/**
+ * Expected dom structure:
+ *  <li class="dynabox">
+      <div class="dynabox_label"><span class="label">Lable Text</span>
+      <ul class="dynabox_content"><li> ...... </li></ul>
+    </li>
+ */
+
+
+Drupal.cdm_dynaboxAutoAttach = function () {
+
+  $('li.dynabox').find('.dynabox_content').hide().click(function(event){event.stopPropagation();});
+  $('li.dynabox span.label').click(
+    function () {
+      var dynabox_content = $(this).toggleClass("dynabox_expanded").parent('li.dynabox').find('.dynabox_content').slideToggle("fast");
+
+      var url = dynabox_content.attr('title');
+      
+      if(url != undefined){
+        dynabox_content.removeAttr('title').find('.loading').css( 'display', 'block');
+	      $.get(url, function(html){
+          dynabox_content.find('.loading').remove().end().append(html);
+        });
+      }
+      
+    });
+    //$('li.dynabox> span').click(function(event){event.stopPropagation();});
+}
+
+
+if (Drupal.jsEnabled) {
+  $(document).ready(Drupal.cdm_dynaboxAutoAttach);
+}
