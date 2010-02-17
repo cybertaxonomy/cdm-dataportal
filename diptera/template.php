0b1593e6b5165444b620eaa2ae42e9bc168722d9
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2010-02-17 12:39:32 +0000

    tagging new release for new edit server

---
+++ diptera/template.php
@@ -0,0 +1,129 @@
+<?php
+// $Id: $
+
+function diptera_cdm_descriptionElements($descriptionElements){
+
+  $outArray = array();
+  $glue = '';
+  $sortOutArray = false;
+  $enclosingHtml = 'ul';
+  
+  // only for diptera
+  if(isset($descriptionElements[0]) && $descriptionElements[0]->feature->uuid == UUID_CITATION ) {
+    foreach($descriptionElements as $element){
+      $tokens = split(":", $element->multilanguageText_L10n->text);
+      if(count($tokens) == 2){
+        // token[0]: taxon name; token[1]: note; 
+        $element->multilanguageText_L10n->text = $tokens[1] . ' [<span class="name">' . $tokens[0] . '</span>]';
+      }
+      if(isset($element->citation->datePublished->start)){
+        $elementMap[partialToYear($element->citation->datePublished->start)] = $element;
+      } else {
+        $elementMap[] = $element;
+      }
+    }
+    $success = ksort($elementMap);
+    $descriptionElements = $elementMap;
+  }
+  // ---
+  
+  foreach($descriptionElements as $element){
+    if($element->class == 'TextData'){
+      $outArray[] = theme('cdm_descriptionElementTextData', $element);
+    }else if($element->class == 'Distribution'){
+      if( !array_search($element->area->representation_L10n, $outArray)){
+        $outArray[] = $element->area->representation_L10n;
+        $glue = ', ';
+        $sortOutArray = true;
+        $enclosingHtml = 'p';
+      }
+    }else{
+      $outArray[] = '<li>No method for rendering unknown description class: '.$element->classType.'</li>';
+    }
+  }
+
+  return theme('cdm_descriptionElementArray', $outArray, $feature, $glue, $sortOutArray, $enclosingHtml);
+}
+
+/**
+ * Allows theaming of the taxon page tabs
+ * 
+ * @param $tabname
+ * @return unknown_type
+ */
+function diptera_cdm_taxonpage_tab($tabname){
+  switch($tabname){
+    case 'Synonymy' : return t('Nomenclature'); break;
+    default : return t($tabname); 
+  }
+}
+
+function diptera_cdm_feature_name($feature_name){
+  switch($feature_name){
+    case "Citation": return t("Citations");
+    default: return t(ucfirst($feature_name));
+  }
+}
+
+
+function diptera_get_partDefinition($nameType){
+  
+  if($nameType == 'ZoologicalName'){
+    return array(
+        'namePart' => array(
+          'name' => true
+        ),
+        'nameAuthorPart' => array(
+          'name' => true,
+          'authors' => true
+        ),
+        'referencePart' => array(
+         'authors' => true,
+         'microreference' => true
+        ),
+        'statusPart' => array(
+          'status' => true,
+        ),
+        'descriptionPart' => array(
+          'description' => true,
+        ),
+      );
+  }
+  return false;
+}
+
+function diptera_get_nameRenderTemplate($renderPath){
+  
+  switch ($renderPath){
+    case 'taxon_page_title': 
+      $template = array(
+          'namePart' => array('#uri'=>true),
+        );
+      break;
+    case  'acceptedFor':
+    case 'list_of_taxa': 
+      $template = array(
+        'namePart' => array('#uri'=>true),
+        'referencePart' => array('#uri'=>true),
+      );
+      break;
+    case 'typedesignations': 
+      $template = array(
+        'namePart' => array('#uri'=>true),
+        'referencePart' => array('#uri'=>true)
+      );
+      break;
+    case 'taxon_page_synonymy':
+    case 'related_taxon':
+    default: 
+      $template = array(
+        'namePart' => array('#uri'=>true),
+        'referencePart' => array('#uri'=>true),
+        'statusPart' => true,
+        'descriptionPart' => true
+      );
+  }
+  return $template;
+}
+
+?>
---
+++ summertime_cdm/template.php
@@ -0,0 +1,171 @@
+<?php
+// $Id: template.php,v 1.1 2009/08/25 19:15:43 troy Exp $
+
+
+/**
+ * Return the different routes names from the pagerouzte database 
+ * without names repetition
+ *
+ * @return
+ *   array of strings with the route names 
+ */
+function _get_route_names(){
+	$route_names = array(); 
+	$result = db_query("SELECT r.path FROM {pageroute_routes} r");
+	while ($data = db_fetch_object($result)) {
+	   if (!in_array($data->path, $route_names)){
+	   $route_names[] = $data->path;	
+	   }
+	}
+	return $route_names;
+}
+
+function _replace_route_title($old_title){
+	$old_titles_list = array('Group' => 'CDM Setup for Groups', 
+	                         'Linux_Community' => 'CDM Setup for Communities', 
+	                         'Windows_Community' => 'CDM Setup for Communities');
+	
+	return $old_titles_list[$old_title];
+}
+
+function _replace_link_title($old_title){
+	$new_title = $old_title;
+	$result = db_query("SELECT  r.path, p.name, p.title FROM {pageroute_routes} r, {pageroute_pages} p WHERE r.prid=p.prid");
+
+	//initialization of route data 
+	while ($data = db_fetch_object($result)) {
+		$route_names[] = $data->path;
+		$route_pages[] = $data->name;
+		$route_titles[] = $data->title;
+	}
+
+	//replacing the route_name for route_title on the tabs
+	if(in_array(arg(0) , $route_names)){
+		$key = array_search($old_title, $route_pages);
+	    if($key != NULL || $key !== FALSE){
+	    	$new_title = $route_titles[$key];
+	    }
+	}
+	
+	return $new_title;
+}
+
+function phptemplate_preprocess_page(&$vars) {
+  $defaults = array(
+    'admin_left_column' => 1,
+    'admin_right_column' => 0
+  );
+  
+  global $theme_key;
+  // Get default theme settings.
+  $settings = theme_get_settings($theme_key);    
+  $settings = array_merge($defaults, $settings);
+ 
+  
+  
+  if (arg(0) == 'admin' && ($settings['admin_right_column'] == 0) && !(arg(1) == 'build' && arg(2) == 'block')) {
+    $vars['right'] = '';
+  }
+  
+  if (arg(0) == 'admin' && ($settings['admin_left_column'] == 0) && !(arg(1) == 'build' && arg(2) == 'block')) {
+    $vars['left'] = '';
+  }
+  
+  $vars['registration_enabled'] = variable_get('user_register', 1);
+  //$vars['closure'] .= '<span class="developer">
+  //<strong><a href="http://russianwebstudio.com" title="Go to RussianWebStudio.com">Drupal theme</a></strong> by        <a href="http://russianwebstudio.com" title="Go to RussianWebStudio.com">RussianWebStudio.com</a> <span class="version">ver.1</span>
+  //</span>';
+}
+
+
+/**
+ * Generate the HTML representing a given menu item ID.
+ *
+ * An implementation of theme_menu_item_link()
+ *
+ * @param $link
+ *   array The menu item to render.
+ * @return
+ *   string The rendered menu item.
+ */
+function phptemplate_menu_item_link($link) {
+  if (empty($link['options'])) {
+    $link['options'] = array();
+  }
+
+  if(strpos($link['title'], arg(1)) !== false){
+    $link['options']['attributes']['class'] = 'active_node';
+  }
+  
+  // If an item is a LOCAL TASK, render it as a tab
+  if ($link['type'] & MENU_IS_LOCAL_TASK) {
+  	$new_title = _replace_link_title($link['title']); 	
+    //$link['title'] = '<span class="tab">' . check_plain($link['title']) . '</span>';
+    $link['title'] = '<span class="tab">' . $new_title . '</span>';
+    $link['options']['html'] = TRUE;
+  }
+
+  if (empty($link['type'])) {
+    $true = TRUE;
+  }
+  
+  $attributes = array();
+
+  return l($link['title'], $link['href'], $link['options']);
+}
+
+/**
+ * Duplicate of theme_menu_local_tasks() but adds clear-block to tabs.
+ */
+function phptemplate_menu_local_tasks() {
+  $output = '';
+  $route_names = _get_route_names();
+  
+  
+  if (($primary = menu_primary_local_tasks()) && ( in_array(arg(0) , $route_names))) {
+  	// it is a page root local task!
+  	$li_count = substr_count($primary, "<li");
+  	
+  	$li_width = floor(100 / $li_count).'%';
+  	$primary = str_replace('<li', '<li style="width: '.$li_width.'"', $primary);
+    
+    $pos = strpos($primary, '<li');
+  	$primary = substr_replace($primary, ' class="first" ', $pos + 3, 1);
+  	$pos = strrpos($primary, '<li');
+    $primary = substr_replace($primary, ' class="last" ', $pos + 3, 1);
+  	
+    //$a = str_replace('_',' ',arg(0));
+    //replacing the navigation banner title
+    $new_title = _replace_route_title(arg(0));
+    $title = '<h2 class="pageroute_title">' .  $new_title . '</h2>'; 
+  	$output .= '<div class="pageroute pageroute_'.strtolower(arg(0)).'">'.$title.'<ul class="tabs primary clear-block">'. $primary .'</ul>'.'</div>';
+  
+  } elseif ($primary = menu_primary_local_tasks()) {
+    $output .= '<ul class="tabs primary clear-block">' . $primary . '</ul>';
+  }
+
+  if ($secondary = menu_secondary_local_tasks()) {
+    $output .= '<ul class="tabs secondary clear-block">' . $secondary . '</ul>';
+  }
+  
+  return $output;
+}
+
+function summertime_cdm_menu_item_link($link) {
+  if (empty($link['localized_options'])) {
+    $link['localized_options'] = array();
+  }
+  
+  $link['localized_options']['html'] = true;
+
+  return l('<span>' . $link['title'] . '</span>', $link['href'], $link['localized_options']);
+}
+
+function summertime_cdm_node_submitted($node) {
+  return t('by !username on @datetime',
+    array(
+      '!username' => theme('username', $node),
+      '@datetime' => format_date($node->created),
+    ));
+}
+
