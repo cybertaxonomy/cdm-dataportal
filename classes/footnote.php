0c4ec72479eeeaebe11f23055d9c83672a8ef224
f.revilla <f.revilla@localhost>
2010-09-13 10:01:38 +0000

    merging revisions r10190 from trunk/modules/cdm_dataportal to branch/modules/cdm_dataportal_realease-2.4.3/

--- classes/footnote.php
+++ classes/footnote.php
@@ -2,21 +2,21 @@
 // $Id$
 
 /**
-* Copyright (C) 2007 EDIT
-* European Distributed Institute of Taxonomy 
-* http://www.e-taxonomy.eu
-* 
-* The contents of this file are subject to the Mozilla Public License Version 1.1
-* See http://www.mozilla.org/MPL/MPL-1.1.html for the full license terms.
-*/
+ * Copyright (C) 2007 EDIT
+ * European Distributed Institute of Taxonomy
+ * http://www.e-taxonomy.eu
+ *
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * See http://www.mozilla.org/MPL/MPL-1.1.html for the full license terms.
+ */
 
 
 class Footnote
 {
-  
+
     public $key, $object, $theme, $themeArguments;
-    
-    
+
+
     // private constructor
     public function __construct($footnoteKey, $object, $theme = null, array $themeArguments = array()) {
         $this->key = $footnoteKey;
@@ -29,12 +29,11 @@ class Footnote
     }
 
     public function doRender(){
-      
+
       if($this->theme){
       $args = $this->themeArguments;
       array_unshift($this->object);
       array_unshift($this->theme);
-
         $out = call_user_func_array('theme', $args);
       } else {
         $out = $this->object;
