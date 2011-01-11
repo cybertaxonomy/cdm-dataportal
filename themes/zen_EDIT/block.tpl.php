15f558c95ae7bdd1aab32ec89dedf0963fb3afcc
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-01-11 12:27:12 +0000

    theme discontinued

--- themes/zen_EDIT/block.tpl.php
+++
@@ -1,8 +0,0 @@
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module ?>">  
-  <div class="blockinner">
-    <h2 class="title"> <?php print $block->subject; ?> </h2>
-    <div class="content">
-      <?php print $block->content; ?>
-    </div>    
-  </div>
-</div>
