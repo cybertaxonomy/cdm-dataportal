d14677944b26a6f71430dcac1803c50eb0513ea4
f.revilla <f.revilla@localhost>
2011-01-26 09:55:38 +0000

    merging with trunk/modules/cdm_dataportal r11200 and r11216

--- cdm_api/uuids.php
+++ cdm_api/uuids.php
@@ -98,6 +98,8 @@ define('UUID_DISCUSSION', 'd3c4cbb6-0025-4322-886b-cd0156753a25');
 define('UUID_IMAGE', '84193b2c-327f-4cce-90ef-c8da18fd5bb5');
 define('UUID_NAME_USAGE', '99b2842f-9aa7-42fa-bd5f-7285311e0101');
 define('UUID_CHROMOSOMES', 'c4a60319-4978-4692-9545-58d60cf8379e');
+define('UUID_CHROMOSOMES_NUMBERS', '6f677e98-d8d5-4bc5-80bf-affdb7e3945a');
+define('UUID_IMAGE_SOURCES', 'c0cc5ebe-1f0c-4c31-af53-d486858ea415');
 
 // typedesignation status
 define('UUID_HOLOTYPE', 'a407dbc7-e60c-46ff-be11-eddf4c5a970d');
