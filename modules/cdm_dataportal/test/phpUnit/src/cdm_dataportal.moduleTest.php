f87d09a92af04a0d76a935b729242b0e01bed4ba
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-04-14 08:48:44 +0000

    benchmark modified: better output

--- modules/cdm_dataportal/test/phpUnit/src/cdm_dataportal.moduleTest.php
+++ modules/cdm_dataportal/test/phpUnit/src/cdm_dataportal.moduleTest.php
@@ -6,49 +6,48 @@ class ModuleTests extends PHPUnit_Framework_TestCase {
 
 		$polytomousKeysPager = cdm_ws_get(CDM_WS_POLYTOMOUSKEY, $polytomousKeyUuid);
 
-		print("\n\nBenchmarking web services: deep initialization vs. bit-by-bit initialization\n"
+		print("\n\n<h4>Benchmarking web services: deep initialization vs. bit-by-bit initialization</h4>\n"
 		. $polytomousKeysPager->count . " PolytomousKeys to load.\n");
 
-		print("1) bit-by-bit initialization ...\n");
+		print("<table border=\"1\">\n<tr><th>key uuid</td><td>bit-by-bit initialization</td><td>deep initialization</td></tr>\n");
 		flush();
-		$time_load_total = 0;
+
+		$time_load_bbb_total = 0;
+		$time_load_deep_total = 0;
 		$i = 0;
 		foreach($polytomousKeysPager->records as $polytomousKey){
+
 			if(! is_uuid($polytomousKey->uuid)){
 				continue;
 			}
+//			if($i++ == 4){
+//				break;
+//			}
+
+			print("<tr><td>$polytomousKey->uuid</td>");
+			// ---- bit-by-bit ---- //
 			$time_load_start = microtime(true);
 			$polytomousKey = cdm_ws_get(CDM_WS_POLYTOMOUSKEY, $polytomousKey->uuid);
 			_load_polytomousKeySubGraph($polytomousKey->root);
 			$time_load = microtime(true) - $time_load_start;
-			print($polytomousKey->uuid . " :\t" . sprintf('%3.3f', $time_load). "s\n");
-			if($i++ == 1){
-				var_dump($polytomousKey);
-			}
-			$time_load_total += $time_load;
-		}
-		flush();
-		print("total time = " . sprintf('%3.3f', $time_load_total) . "s\n");
+//			if($i++ == 1){
+//				var_dump($polytomousKey);
+//			}
+			$time_load_bbb_total += $time_load;
+      print("<td>" . sprintf('%3.3f', $time_load) . "s</td>");
 
-
-		print("\n2) deep initialization ...\n");
-		flush();
-		$time_load_total = 0;
-		$i = 0;
-		foreach($polytomousKeysPager->records as $polytomousKey){
-		  if(! is_uuid($polytomousKey->uuid)){
-        continue;
-      }
+      // ---- deep ---- //
       $time_load_start = microtime(true);
 			$polytomousKey = cdm_ws_get("portal/" . CDM_WS_POLYTOMOUSKEY, array($polytomousKey->uuid, "loadWithNodes"));
 			$time_load = microtime(true) - $time_load_start;
-			print($polytomousKey->uuid . " :\t" . sprintf('%3.3f', $time_load). "s\n");
-			if($i++ == 1){
-        var_dump($polytomousKey);
-      }
-			$time_load_total += $time_load;
+//			if($i++ == 1){
+//        var_dump($polytomousKey);
+//      }
+			$time_load_deep_total += $time_load;
+      print("<td>" . sprintf('%3.3f', $time_load) . "s</td></tr>\n");
+
 		}
-		print("total time = " . sprintf('%3.3f', $time_load_total) . "s\n");
+	  print("<tr><td>total time</td><td>" . sprintf('%3.3f', $time_load_bbb_total) . "s</td><td>" . sprintf('%3.3f', $time_load_deep_total) . "s</td></tr>\n");
 		flush();
 
 	}
