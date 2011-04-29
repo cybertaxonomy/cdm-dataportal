d8f3e57afe91d858733e529abf9a345f915665b0
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-04-29 15:05:02 +0000

    maven project for selenium tests

---
+++ modules/cdm_dataportal/test/java/dataportal-selenium-tests/src/main/java/eu/etaxonomy/dataportal/selenium/App.java
@@ -0,0 +1,13 @@
+package eu.etaxonomy.dataportal.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
