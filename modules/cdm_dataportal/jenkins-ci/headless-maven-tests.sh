87618c3be0139d8c4a2de420ee9e6e3edd85df6c
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-05-02 16:06:32 +0000

    script for runing headless maven tests

--- modules/cdm_dataportal/jenkins-ci/headless-maven-tests.sh
+++ modules/cdm_dataportal/jenkins-ci/headless-maven-tests.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #
+# USAGE:
+#     bash -ex /usr/lib/selenium/headlessSelenium.sh $WORKSPACE 
 #
 #
+WORKSPACE=$1
+FIREFOX_BIN="/usr/lib/iceweasel/firefox-bin"
+
 DISPLAY=":99"
-# 
+
+#
 # This option creates screen screennum and sets its width, height, and depth to W, H, and  D  respectively.
 # By default, only screen 0 exists and has the dimensions 1280x1024x8.
 #
@@ -15,5 +21,10 @@ VIRTUAL_X="Xvfb $DISPLAY"
 #init
 if [ -z "$(pidof Xvfb)" ]; then
         $VIRTUAL_X &
+else
+        echo "Xvfb already running with pid $(pidof Xvfb)"
 fi
 export DISPLAY
+
+cd $WORKSPACE/test/java/dataportal-selenium-tests/
+mvn -Dwebdriver.firefox.bin=$FIREFOX_BIN test
