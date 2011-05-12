3d973c2b514f41d3421e216a107821033db1db76
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-05-12 13:08:11 +0000

    new release:  RELEASE-3.0.4

---
+++ summertime_cdm/box.tpl.php
@@ -0,0 +1,8 @@
+<?php
+// $Id: box.tpl.php,v 1.1 2009/08/25 19:15:43 troy Exp $
+?>
+  <div class="box">
+    <?php if ($title) { ?><h2 class="title"><?php print $title; ?></h2><?php } ?>
+    <div class="content"><?php print $content; ?></div>
+ </div>
+
