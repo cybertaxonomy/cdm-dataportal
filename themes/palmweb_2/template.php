3d143b3346135256b4f50764c0c5b5c19a280399
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-06-23 15:20:53 +0000

    new sub module fsi_gallery - fixing: #2469 (FSI gallery copyright statement configurable),  #2386 (FSI gallery query configurable)

--- themes/palmweb_2/template.php
+++ themes/palmweb_2/template.php
@@ -30,65 +30,6 @@ function palmweb_2_cdm_taxon_page_description($taxon, $mergedTrees, $media, $hid
   return $out;
 }
 
-function _disabled_palmweb_2_cdm_taxon_page_images($taxon, $media){
-
-  $flashLink = isset($media[0]);
-
-  if($flashLink){
-
-    $taggedName = $taxon->name->taggedName;
-
-    $nameArray = array();
-    foreach($taggedName as $taggedText){
-      if($taggedText->type == 'name'){
-        $nameArray[] = $taggedText->text;
-      }
-    }
-
-   $query = join("%5F", $nameArray) . '%20AND%20EditWP6%20AND%20jpg';
-
-  $out = '
-
-  <script type="text/javascript" src="http://media.bgbm.org/erez/js/fsiwriter.js"></script>
-
-<script type="text/javascript">
-<!--
-	writeFlashCode( "http://media.bgbm.org/erez/fsi/fsi.swf?&cfg=showcase_presets/showcase_info.fsi&effects=%26quality%3D95&showcase_query='.$query.'&skin=silver&showcase_labeltextheight=50&textbox_textfrom=IPTC_WP6&textbox_height=50&param_backgroundcolor=454343&publishwmode=opaque&showcase_hscroll=true&showcase_basecolor=454343&plugins=PrintSave,textbox",
-		"http://media.bgbm.org/erez/erez?src=erez-private/flashrequired.svg&tmp=Large&quality=97&width=500&height=323",
-		"width=500;height=323;bgcolor=454343;wmode=opaque");
-// -->
-</script>
-<noscript>
-	<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,65,0" width="500" height="323">
-		<param name="movie" value="http://media.bgbm.org/erez/fsi/fsi.swf?&cfg=showcase_presets/showcase_info.fsi&effects=%26quality%3D95&showcase_query='.$query.'&skin=silver&showcase_labeltextheight=50&textbox_textfrom=IPTC_WP6&textbox_height=50&param_backgroundcolor=454343&publishwmode=opaque&showcase_hscroll=true&showcase_basecolor=454343plugins=PrintSave,textbox"/>
-		<param name="bgcolor" value="454343" />
-		<param name="wmode" value="opaque" />
-		<param name="allowscriptaccess" value="always" />
-		<param name="allowfullscreen" value="true" />
-		<param name="quality" value="high" />
-		<embed src="http://media.bgbm.org/erez/fsi/fsi.swf?&cfg=showcase_presets/showcase_info.fsi&effects=%26quality%3D95&showcase_query='.$query.'&skin=silver&showcase_labeltextheight=50&textbox_textfrom=IPTC_WP6&textbox_height=50&param_backgroundcolor=454343&publishwmode=opaque&showcase_hscroll=true&showcase_basecolor=454343plugins=PrintSave,textbox"
-			width="500"
-			height="323"
-			bgcolor="454343"
-			wmode="opaque"
-			allowscriptaccess="always"
-			allowfullscreen="true"
-			quality="high"
-			type="application/x-shockwave-flash"
-			pluginspage="http://www.adobe.com/go/getflashplayer">
-		</embed>
-	</object>
-
-</noscript>';
-
-  }else{
-    $out = 'No images available.';
-
-  }
-  return $out;
-
-
-}
 
 function palmweb_2_cdm_descriptionElementDistribution($descriptionElements) {
 
