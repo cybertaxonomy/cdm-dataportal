d124d729d9c9c154c6aa5efab2dc687992cda6a0
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-09-23 11:38:24 +0000

    fixing broken test

--- modules/cdm_dataportal/test/java/dataportal-selenium-tests/src/test/java/eu/etaxonomy/dataportal/selenium/tests/cyprus/CyprusSearchTest.java
+++ modules/cdm_dataportal/test/java/dataportal-selenium-tests/src/test/java/eu/etaxonomy/dataportal/selenium/tests/cyprus/CyprusSearchTest.java
@@ -9,15 +9,21 @@
  */
 package eu.etaxonomy.dataportal.selenium.tests.cyprus;
 
+import static org.junit.Assert.assertNull;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import eu.etaxonomy.dataportal.DataPortalContext;
+import eu.etaxonomy.dataportal.elements.TaxonListElement;
 import eu.etaxonomy.dataportal.junit.CdmDataPortalTestBase;
 import eu.etaxonomy.dataportal.junit.DataPortalContextSuite.DataPortalContexts;
+import eu.etaxonomy.dataportal.pages.GenericPortalPage;
+import eu.etaxonomy.dataportal.pages.TaxonSearchResultPage;
 
 /**
  * @author a.kohlbecker
@@ -27,21 +33,29 @@ import eu.etaxonomy.dataportal.junit.DataPortalContextSuite.DataPortalContexts;
 @DataPortalContexts( { DataPortalContext.cyprus })
 public class CyprusSearchTest extends CdmDataPortalTestBase{
 
+	private GenericPortalPage homePage;
+
+	@Before
+	public void setUp() throws Exception {
+
+		driver.get(getContext().getBaseUri().toString());
+		homePage = new GenericPortalPage(driver, getContext());
+
+	}
+
 	/**
 	 * see http://dev.e-taxonomy.eu/trac/ticket/2350
+	 * @throws Exception
 	 */
 	@Test
-	@Ignore
-	public void searchResultsWithoutAnnotationFootnotes(){
-		driver.get(getBaseUrl() + "?query=Genis*&search[tree]=0c2b5d25-7b15-4401-8b51-dd4be0ee5cab&q=cdm_dataportal%2Fsearch%2Ftaxon&search[pageSize]=25&search[pageNumber]=0&search[doTaxa]=1&search[doSynonyms]=1&search[doTaxaByCommonNames]=0");
+	public void searchResultsWithoutAnnotationFootnotes() throws Exception{
 
-		// --- This variant is slower in FF that using the full xpath
-		//		WebElement container = driver.findElement(By.id("squeeze"));
-		//		RenderedWebElement element = (RenderedWebElement)container.findElement(By.xpath("div/div/ul/li/span[contains(@class, 'footnote-key')]"));
+		TaxonSearchResultPage searchResultPage = homePage.submitQuery("Genis*");
 
-		// --- using the full xpath
-		WebElement element = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div/ul/li/span[contains(@class, 'footnote-key')]"));
-		Assert.assertNull("result set entries must not have footnote keys", element);
+		for (TaxonListElement item : searchResultPage.getResultItems()){
+			 assertNull("result set entries must not have footnote keys", item.getElement().findElements(By.className("footnote-key")));
+		}
+
 	}
 
 }
