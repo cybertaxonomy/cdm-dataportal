6a5f5bdb308d186e8e30d0e84ce64cd2805358d5
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-10-04 11:28:25 +0000

    test for #2612: name page broken

--- modules/cdm_dataportal/test/java/dataportal-selenium-tests/src/main/java/eu/etaxonomy/dataportal/pages/TaxonSynonymyPage.java
+++ modules/cdm_dataportal/test/java/dataportal-selenium-tests/src/main/java/eu/etaxonomy/dataportal/pages/TaxonSynonymyPage.java
@@ -43,130 +43,154 @@ import eu.etaxonomy.dataportal.elements.LinkElement;
  */
 public class TaxonSynonymyPage extends PortalPage {
 
-	public static final Logger logger = Logger.getLogger(TaxonSynonymyPage.class);
+    public static final Logger logger = Logger.getLogger(TaxonSynonymyPage.class);
 
-	private UUID taxonUuid;
+    private UUID taxonUuid;
 
-	protected static String drupalPagePathBase = "cdm_dataportal/taxon";
+    protected static String drupalPagePathBase = "cdm_dataportal/taxon";
 
-	/* (non-Javadoc)
-	 * @see eu.etaxonomy.dataportal.pages.PortalPage#getDrupalPageBase()
-	 */
-	@Override
-	protected String getDrupalPageBase() {
-		return drupalPagePathBase;
-	}
+    /* (non-Javadoc)
+     * @see eu.etaxonomy.dataportal.pages.PortalPage#getDrupalPageBase()
+     */
+    @Override
+    protected String getDrupalPageBase() {
+        return drupalPagePathBase;
+    }
 
-	@FindBy(id = "synonymy")
-	@CacheLookup
-	private WebElement synonymy;
-
-
-
-	/**
-	 * @param driver
-	 * @param context
-	 * @param taxonUuid
-	 * @throws MalformedURLException
-	 */
-	public TaxonSynonymyPage(WebDriver driver, DataPortalContext context, UUID taxonUuid) throws MalformedURLException {
-
-		super(driver, context, taxonUuid.toString() + "/synonymy");
-
-		this.taxonUuid = taxonUuid;
-	}
-
-
-	/**
-	 * @param driver
-	 * @param context
-	 * @throws Exception
-	 */
-	public TaxonSynonymyPage(WebDriver driver, DataPortalContext context) throws Exception {
-		super(driver, context);
-	}
-
-
-	/**
-	 * Returns the profile image of the taxon profile page. This image is
-	 * located at the top of the page. The Profile Image can be disabled in the
-	 * DataPortal settings.
-	 *
-	 * @return The Url of the profile image or null if the image is not visible.
-	 */
-	public String getAcceptedName() {
-		WebElement acceptedName = synonymy.findElement(
-				By.xpath("./span[contains(@class,'accepted-name')]")
-		);
-		return acceptedName.getText();
-	}
-
-	public List<LinkElement> getAcceptedNameFootNoteKeys() {
-		List<WebElement> fnkListElements = synonymy.findElements(
-				By.xpath("./span[contains(@class,'accepted-name')]/following-sibling::span[contains(@class, 'footnote-key')]/a")
-		);
-		return ElementUtils.linkElementsFromFootNoteKeyListElements(fnkListElements);
-	}
-
-	/**
-	 * @param synonymIndex
-	 *            the 1-based position of the synonym in the list of homotypical
-	 *            synonyms
-	 * @return the full text line of the synonym including the prepending symbol
-	 *         and all information rendered after the name. All whitespace is
-	 *         normalized to the SPACE character.
-	 */
-	public String getHomotypicalGroupSynonymName(Integer synonymIndex) {
-		WebElement acceptedName = synonymy.findElement(
-				By.xpath("./ul[contains(@class,'homotypicSynonyms')]/li[" + synonymIndex + "]")
-		);
-		return acceptedName.getText().replaceAll("\\s", " ");
-	}
-
-	public List<LinkElement> getHomotypicalGroupFootNoteKeys() {
-		List<WebElement> fnkListElements = synonymy.findElements(
-				By.xpath("./ul[contains(@class,'homotypicSynonyms')]/*/span[contains(@class, 'footnote-key')]/a")
-		);
-		return ElementUtils.linkElementsFromFootNoteKeyListElements(fnkListElements);
-	}
-
-	public List<BaseElement> getHomotypicalGroupFootNotes() {
-		List<WebElement> fnListElements = synonymy.findElements(
-				By.xpath("./ul[contains(@class,'homotypicSynonyms')]/li[contains(@class, 'footnotes')]/span[contains(@class, 'footnote')]")
-		);
-		return ElementUtils.baseElementsFromFootNoteListElements(fnListElements);
-
-	}
+    @FindBy(id = "synonymy")
+    @CacheLookup
+    private WebElement synonymy;
 
 
 
-	/**
-	 * @param heterotypicalGroupIndex
-	 *            the 0-based index of the heterotypical group
-	 * @param synonymIndex
-	 *            the 1-based position of the synonym in the list specified
-	 *            group of heterotypical synonyms
-	 * @return the full text line of the synonym including the prepending symbol
-	 *         and all information rendered after the name. All whitespace is
-	 *         normalized to the SPACE character.
-	 */
-	public String getHeterotypicalGroupSynonymName(Integer heterotypicalGroupIndex, Integer synonymIndex) {
-		WebElement acceptedName = synonymy.findElement(By.xpath("./ul[contains(@class,'heterotypicSynonymyGroup')][" + heterotypicalGroupIndex + "]/li[" + synonymIndex + "]"));
-		return acceptedName.getText().replaceAll("\\s", " ");
-	}
+    /**
+     * @param driver
+     * @param context
+     * @param taxonUuid
+     * @throws MalformedURLException
+     */
+    public TaxonSynonymyPage(WebDriver driver, DataPortalContext context, UUID taxonUuid) throws MalformedURLException {
 
-	public List<LinkElement> getHeterotypicalGroupFootNoteKeys(Integer heterotypicalGroupIndex) {
-		List<WebElement> fnkListElements = synonymy.findElements(
-				By.xpath("./ul[contains(@class,'heterotypicSynonymyGroup')][" + heterotypicalGroupIndex + "]/*/span[contains(@class, 'footnote-key')]/a")
-		);
-		return ElementUtils.linkElementsFromFootNoteKeyListElements(fnkListElements);
-	}
+        super(driver, context, taxonUuid.toString() + "/synonymy");
 
-	public List<BaseElement> getHeterotypicalGroupFootNotes(Integer heterotypicalGroupIndex) {
-		List<WebElement> fnListElements = synonymy.findElements(
-				By.xpath("./ul[contains(@class,'heterotypicSynonymyGroup')][" + heterotypicalGroupIndex + "]/li[contains(@class, 'footnotes')]/span[contains(@class, 'footnotes')]")
-		);
-		return ElementUtils.baseElementsFromFootNoteListElements(fnListElements);
-	}
+        this.taxonUuid = taxonUuid;
+    }
+
+
+    /**
+     * @param driver
+     * @param context
+     * @throws Exception
+     */
+    public TaxonSynonymyPage(WebDriver driver, DataPortalContext context) throws Exception {
+        super(driver, context);
+    }
+
+
+    /**
+     * Returns the profile image of the taxon profile page. This image is
+     * located at the top of the page. The Profile Image can be disabled in the
+     * DataPortal settings.
+     *
+     * @return The Url of the profile image or null if the image is not visible.
+     */
+    public String getAcceptedName() {
+        WebElement acceptedName = synonymy.findElement(
+                By.xpath("./span[contains(@class,'accepted-name')]")
+        );
+        return acceptedName.getText();
+    }
+
+    public List<LinkElement> getAcceptedNameFootNoteKeys() {
+        List<WebElement> fnkListElements = synonymy.findElements(
+                By.xpath("./span[contains(@class,'accepted-name')]/following-sibling::span[contains(@class, 'footnote-key')]/a")
+        );
+        return ElementUtils.linkElementsFromFootNoteKeyListElements(fnkListElements);
+    }
+
+    /**
+     * @param synonymIndex
+     *            the 1-based position of the synonym in the list of homotypical
+     *            synonyms
+     * @return the full text line of the synonym including the prepending symbol
+     *         and all information rendered after the name. All whitespace is
+     *         normalized to the SPACE character.
+     */
+    public String getHomotypicalGroupSynonymName(Integer synonymIndex) {
+        WebElement synonym = getHomotypicalGroupSynonym(synonymIndex);
+        return synonym.getText().replaceAll("\\s", " ");
+    }
+
+
+    /**
+     * @param synonymIndex
+     * @return
+     */
+    public WebElement getHomotypicalGroupSynonym(Integer synonymIndex) {
+        WebElement synonym = synonymy.findElement(
+                By.xpath("./ul[contains(@class,'homotypicSynonyms')]/li[" + synonymIndex + "]")
+        );
+        return synonym;
+    }
+
+    public List<LinkElement> getHomotypicalGroupFootNoteKeys() {
+        List<WebElement> fnkListElements = synonymy.findElements(
+                By.xpath("./ul[contains(@class,'homotypicSynonyms')]/*/span[contains(@class, 'footnote-key')]/a")
+        );
+        return ElementUtils.linkElementsFromFootNoteKeyListElements(fnkListElements);
+    }
+
+    public List<BaseElement> getHomotypicalGroupFootNotes() {
+        List<WebElement> fnListElements = synonymy.findElements(
+                By.xpath("./ul[contains(@class,'homotypicSynonyms')]/li[contains(@class, 'footnotes')]/span[contains(@class, 'footnote')]")
+        );
+        return ElementUtils.baseElementsFromFootNoteListElements(fnListElements);
+
+    }
+
+
+
+    /**
+     * @param heterotypicalGroupIndex
+     *            the 1-based index of the heterotypical group
+     * @param synonymIndex
+     *            the 1-based position of the synonym in the list specified
+     *            group of heterotypical synonyms
+     * @return the full text line of the synonym including the prepending symbol
+     *         and all information rendered after the name. All whitespace is
+     *         normalized to the SPACE character.
+     */
+    public String getHeterotypicalGroupSynonymName(Integer heterotypicalGroupIndex, Integer synonymIndex) {
+        WebElement synonym = getHeterotypicalGroupSynonym(heterotypicalGroupIndex, synonymIndex);
+        return synonym.getText().replaceAll("\\s", " ");
+    }
+
+
+    /**
+     * @param heterotypicalGroupIndex
+     *            the 1-based index of the heterotypical group
+     * @param synonymIndex
+     *            the 1-based position of the synonym in the list specified
+     *            group of heterotypical synonyms
+     * @return
+     */
+    public WebElement getHeterotypicalGroupSynonym(Integer heterotypicalGroupIndex, Integer synonymIndex) {
+        WebElement synonym = synonymy.findElement(By.xpath("./ul[contains(@class,'heterotypicSynonymyGroup')][" + heterotypicalGroupIndex + "]/li[" + synonymIndex + "]"));
+        return synonym;
+    }
+
+    public List<LinkElement> getHeterotypicalGroupFootNoteKeys(Integer heterotypicalGroupIndex) {
+        List<WebElement> fnkListElements = synonymy.findElements(
+                By.xpath("./ul[contains(@class,'heterotypicSynonymyGroup')][" + heterotypicalGroupIndex + "]/*/span[contains(@class, 'footnote-key')]/a")
+        );
+        return ElementUtils.linkElementsFromFootNoteKeyListElements(fnkListElements);
+    }
+
+    public List<BaseElement> getHeterotypicalGroupFootNotes(Integer heterotypicalGroupIndex) {
+        List<WebElement> fnListElements = synonymy.findElements(
+                By.xpath("./ul[contains(@class,'heterotypicSynonymyGroup')][" + heterotypicalGroupIndex + "]/li[contains(@class, 'footnotes')]/span[contains(@class, 'footnotes')]")
+        );
+        return ElementUtils.baseElementsFromFootNoteListElements(fnListElements);
+    }
 
 }
--- modules/cdm_dataportal/test/java/dataportal-selenium-tests/src/test/java/eu/etaxonomy/dataportal/selenium/tests/cichorieae/Cichorieae_NamePageTest.java
+++ modules/cdm_dataportal/test/java/dataportal-selenium-tests/src/test/java/eu/etaxonomy/dataportal/selenium/tests/cichorieae/Cichorieae_NamePageTest.java
@@ -9,12 +9,14 @@
  */
 package eu.etaxonomy.dataportal.selenium.tests.cichorieae;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.net.MalformedURLException;
 import java.util.UUID;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import eu.etaxonomy.dataportal.DataPortalContext;
 import eu.etaxonomy.dataportal.junit.CdmDataPortalTestBase;
@@ -32,7 +34,9 @@ import eu.etaxonomy.dataportal.pages.TaxonSynonymyPage;
 public class Cichorieae_NamePageTest extends CdmDataPortalTestBase{
 
 
-	static UUID intybellia_rosea_cass_Uuid = UUID.fromString("b8f725f0-320a-49e5-aa9a-82cef1c47c17");
+    static UUID intybellia_rosea_cass_Uuid = UUID.fromString("b8f725f0-320a-49e5-aa9a-82cef1c47c17");
+
+    static UUID lapsana_zacintha_Uuid = UUID.fromString("fee25b71-537b-48b5-a18d-7c863000c7af");
 
 //	static UUID lactuca_favratii_Uuid = UUID.fromString("6027e1fa-9fe5-4ddc-a2de-f72bfa7378c0");
 //
@@ -41,11 +45,24 @@ public class Cichorieae_NamePageTest extends CdmDataPortalTestBase{
 //	static UUID crepis_artificialis_Uuid = UUID.fromString("3eabdf89-ddeb-461c-b6f8-341bb8deb7bf");
 
 
-	@Test
-	public void intybellia_rosea_cass() throws MalformedURLException {
-		GenericPortalPage p = new GenericPortalPage(driver, getContext(), "name/" + intybellia_rosea_cass_Uuid);
-		assertEquals(getContext().prepareTitle("Crepis purpurea"), p.getTitle());
-	}
+    @Test
+    public void intybellia_rosea_cass() throws Exception {
+        GenericPortalPage p = new GenericPortalPage(driver, getContext(), "name/" + intybellia_rosea_cass_Uuid);
+        assertEquals(getContext().prepareTitle("Crepis purpurea"), p.getTitle());
+        TaxonSynonymyPage synonymyPage = new TaxonSynonymyPage(driver, getContext());
+        WebElement intybellia_rosea_cass_synonym = synonymyPage.getHeterotypicalGroupSynonym(1, 1);
+        assertNotNull("Synonym 'Intybellia rosea Cass.' should be highlighted", intybellia_rosea_cass_synonym.findElement(By.className("highlite")));
+
+    }
+
+    @Test
+    public void lapsana_zacintha_L() throws Exception {
+        GenericPortalPage p = new GenericPortalPage(driver, getContext(), "name/" + lapsana_zacintha_Uuid);
+        assertEquals(getContext().prepareTitle("Crepis zacintha"), p.getTitle());
+        TaxonSynonymyPage synonymyPage = new TaxonSynonymyPage(driver, getContext());
+        WebElement lapsana_zacintha_synonym = synonymyPage.getHomotypicalGroupSynonym(1);
+        assertNotNull("Synonym 'Lapsana zacintha L.' should be highlighted", lapsana_zacintha_synonym.findElement(By.className("highlite")));
+    }
 
 
 }
