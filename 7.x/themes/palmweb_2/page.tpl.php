14fa6a3ae6e9af83e5d69b95194c6b674ea01aff
w.addink <w.addink@localhost>
2012-07-14 17:22:17 +0000

    solved an issue with location of tabs

--- 7.x/themes/palmweb_2/page.tpl.php
+++ 7.x/themes/palmweb_2/page.tpl.php
@@ -44,13 +44,15 @@
           <?php //print $breadcrumb; ?>
           <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
           <a id="main-content"></a>
-          <?php if ($tabs): ?><div id="tabs-wrapper" class="clearfix"><?php endif; ?>
+          <?php // unfortunately in D7 tabs are always set, even if there are none.. ?>
+          <?php if ($tabs = render($tabs)): ?><div id="tabs-wrapper" class="clearfix"><?php endif; ?>
+          
           <?php print render($title_prefix); ?>
           <?php if ($title): ?>
-            <h2<?php print $tabs ? ' class="with-tabs"' : '' ?>><?php print $title ?></h2>
+            <h2<?php if($tabs = render($tabs)):?><?php print ' class="with-tabs"' ; ?><?php endif; ?>><?php print $title ?></h2>
           <?php endif; ?>
           <?php print render($title_suffix); ?>
-          <?php if ($tabs): ?><?php print render($tabs); ?></div><?php endif; ?>
+          <?php if ($tabs = render($tabs)): ?><?php print render($tabs); ?></div><?php endif; ?>
           <?php print render($tabs2); ?>
           <?php print render($page['help']); ?>
           <?php print $messages; ?>
