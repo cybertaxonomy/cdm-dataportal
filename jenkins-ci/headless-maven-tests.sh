105c593566e3eb672c48c2e4d8a338a43f3392bc
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2012-07-23 08:49:30 +0000

    this branch will take important bugfix change sets to be merged into   branches/drupal/dataportal-to-drupal7

---
+++ jenkins-ci/headless-maven-tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# USAGE:
+#     bash -ex /usr/lib/selenium/headlessSelenium.sh MODULE_ROOT
+# whereas MODULE_ROOT points to the cdm_dataportal folder in the jenkins workspace
+#
+MODULE_ROOT=$1
+FIREFOX_BIN="/usr/lib/iceweasel/firefox-bin"
+
+DISPLAY=":99"
+
+#
+# This option creates screen screennum and sets its width, height, and depth to W, H, and  D  respectively.
+# By default, only screen 0 exists and has the dimensions 1280x1024x8.
+#
+SCREEN="0 1280x1024x16"
+
+#Use virtual X server
+VIRTUAL_X="Xvfb $DISPLAY"
+
+#init
+if [ -z "$(pidof Xvfb)" ]; then
+        $VIRTUAL_X &
+else
+        echo "Xvfb already running with pid $(pidof Xvfb)"
+fi
+export DISPLAY
+
+cd $MODULE_ROOT/test/java/dataportal-selenium-tests/
+mvn -Dwebdriver.firefox.bin=$FIREFOX_BIN test
