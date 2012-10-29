3df5049b71a871007c930292df9d922d5624f6a1
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2012-10-29 17:21:47 +0000

    updating tests to Drupal 7 specialties and to latest selenium version

--- 7.x/modules/cdm_dataportal/test/java/dataportal-selenium-tests/src/test/java/eu/etaxonomy/dataportal/selenium/tests/cyprus/Allium_guttatum_subsp_guttatum_TaxonProfileTest.java
+++ 7.x/modules/cdm_dataportal/test/java/dataportal-selenium-tests/src/test/java/eu/etaxonomy/dataportal/selenium/tests/cyprus/Allium_guttatum_subsp_guttatum_TaxonProfileTest.java
@@ -63,7 +63,7 @@ public class Allium_guttatum_subsp_guttatum_TaxonProfileTest extends CdmDataPort
 
         List<LinkElement> primaryTabs = p.getPrimaryTabs();
         assertEquals("Expecting 3 tabs", 3, primaryTabs.size());
-        assertEquals("General", primaryTabs.get(0).getText());
+        assertEquals("General\n(active tab)", primaryTabs.get(0).getText());
         assertEquals("Synonymy", primaryTabs.get(1).getText());
         assertEquals("Images", primaryTabs.get(2).getText());
 
@@ -107,8 +107,8 @@ public class Allium_guttatum_subsp_guttatum_TaxonProfileTest extends CdmDataPort
         assertTrue("expecting one footnote 0 to be the footnote for key 0",footNote_1.getText().startsWith(footNoteKey_1.getText()));
 
         p.hover(footNoteKey_1.getElement());
-        assertEquals("rgba(255,255,0,1)", footNoteKey_1.getElement().getCssValue("background-color"));
-        assertEquals("rgba(255,255,0,1)", footNote_1.getElement().getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 0, 1)", footNoteKey_1.getElement().getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 0, 1)", footNote_1.getElement().getCssValue("background-color"));
 
         assertEquals("1. R. D. Meikle, Flora of Cyprus 2. 1985", footNote_1.getText());
 
