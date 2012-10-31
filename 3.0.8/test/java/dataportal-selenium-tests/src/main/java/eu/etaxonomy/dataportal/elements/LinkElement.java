2a431909817943a0180ecee6391bb41dd16307e0
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2012-10-31 11:07:53 +0000

    tag for dataportal release 3.0.8

---
+++ 3.0.8/test/java/dataportal-selenium-tests/src/main/java/eu/etaxonomy/dataportal/elements/LinkElement.java
@@ -0,0 +1,52 @@
+// $Id$
+/**
+ * Copyright (C) 2011 EDIT
+ * European Distributed Institute of Taxonomy
+ * http://www.e-taxonomy.eu
+ *
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * See LICENSE.TXT at the top of this package for the full license terms.
+ */
+
+package eu.etaxonomy.dataportal.elements;
+
+import org.openqa.selenium.WebElement;
+
+/**
+ * @author andreas
+ * @date Jul 1, 2011
+ *
+ */
+public class LinkElement extends BaseElement {
+
+	private String href = null;
+	public String getUrl() {
+		return href;
+	}
+
+	public void setHref(String href) {
+		this.href = href;
+	}
+
+
+	public LinkElement(WebElement a) {
+
+		super(a);
+
+		if(!a.getTagName().equals("a")){
+			throw new RuntimeException("Invalid WebElement: <" + a.getTagName() + "> given, but must be <a>");
+		}
+
+		// read src url
+		if (a.getAttribute("href") != null) {
+				setHref(a.getAttribute("href"));
+		}
+
+	}
+
+	@Override
+	public String toSting(){
+		return super.toSting() + ": " + getUrl() + "";
+	}
+
+}
---
+++ 3.0.8/test/java/dataportal-selenium-tests/src/test/java/eu/etaxonomy/dataportal/selenium/tests/cyprus/CyprusSearchTest.java
@@ -0,0 +1,47 @@
+// $Id$
+/**
+ * Copyright (C) 2009 EDIT
+ * European Distributed Institute of Taxonomy
+ * http://www.e-taxonomy.eu
+ *
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * See LICENSE.TXT at the top of this package for the full license terms.
+ */
+package eu.etaxonomy.dataportal.selenium.tests.cyprus;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import eu.etaxonomy.dataportal.DataPortalContext;
+import eu.etaxonomy.dataportal.junit.CdmDataPortalTestBase;
+import eu.etaxonomy.dataportal.junit.DataPortalContextSuite.DataPortalContexts;
+
+/**
+ * @author a.kohlbecker
+ *
+ */
+
+@DataPortalContexts( { DataPortalContext.cyprus })
+public class CyprusSearchTest extends CdmDataPortalTestBase{
+
+	/**
+	 * see http://dev.e-taxonomy.eu/trac/ticket/2350
+	 */
+	@Test
+	@Ignore
+	public void searchResultsWithoutAnnotationFootnotes(){
+		driver.get(getBaseUrl() + "?query=Genis*&search[tree]=0c2b5d25-7b15-4401-8b51-dd4be0ee5cab&q=cdm_dataportal%2Fsearch%2Ftaxon&search[pageSize]=25&search[pageNumber]=0&search[doTaxa]=1&search[doSynonyms]=1&search[doTaxaByCommonNames]=0");
+
+		// --- This variant is slower in FF that using the full xpath
+		//		WebElement container = driver.findElement(By.id("squeeze"));
+		//		RenderedWebElement element = (RenderedWebElement)container.findElement(By.xpath("div/div/ul/li/span[contains(@class, 'footnote-key')]"));
+
+		// --- using the full xpath
+		WebElement element = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div/div/div/ul/li/span[contains(@class, 'footnote-key')]"));
+		Assert.assertNull("result set entries must not have footnote keys", element);
+	}
+
+}
