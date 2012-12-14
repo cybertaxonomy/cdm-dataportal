56df73fdb044896a67aedee4a7fa5d1cb4894840
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2012-12-14 13:22:36 +0000

    fixing js problems

--- 7.x/modules/cdm_dataportal/js/cdm_dynabox.js
+++ 7.x/modules/cdm_dataportal/js/cdm_dynabox.js
@@ -1,36 +1,50 @@
 /**
  * Expected dom structure:
  *  <li class="dynabox">
-      <div class="dynabox_label"><span class="label">Lable Text</span>
-      <ul class="dynabox_content"><li> ...... </li></ul>
+      <div class="dynabox_label"><span class="label">Label Text</span>
+      <ul class="dynabox_content" title="{url-to-content}"><li> ...... </li></ul>
     </li>
  */
 
 
-(Drupal.cdm_dynaboxAutoAttach = (function ($) {
-
-  var loadDynaContent =  function(event) {
-    event.preventDefault(); //Cancel the default action (navigation) of the click.
-    var dynabox_content = $(this).toggleClass("dynabox_expanded").parent('.dynabox').find('.dynabox_content').slideToggle("fast");
-
-    var url = dynabox_content.attr('title');
-
-    if(url !== undefined && url.length > 1){
-      dynabox_content.removeAttr('title').find('.loading').show();
-      $.get(url, function(html){
-        dynabox_content.find('.loading').remove().end().append(html);
-      });
-    }
-  };
-
-  $('.dynabox').find('.dynabox_content').click(function(event){event.stopPropagation();});
-
-  $('.dynabox a.label').dblclick(loadDynaContent);
-  $('.dynabox a.label').click(loadDynaContent);
-
-    //$('li.dynabox> span').click(function(event){event.stopPropagation();});
-}))(jQuery);
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function($){
+	this.dynabox = function() {
 
 
+		$('.dynabox .label').click(function(event) {
+			loadContent(event);
+		});
 
-jQuery(document).ready(Drupal.cdm_dynaboxAutoAttach);
+		$('.dynabox').find('.dynabox_content').click(function(event){event.stopPropagation();});
+
+		//$('li.dynabox> span').click(function(event){event.stopPropagation();});
+
+		var loadContent = function(event) {
+			event.preventDefault(); //Cancel the default action (navigation) of the click.
+			var dynabox_content = $(event.target).parent('.dynabox').find('.dynabox_content');
+
+			if(dynabox_content.find('.content').length > 0) {
+				dynabox_content.slideToggle("fast");
+			} else {
+				var url = dynabox_content.attr('title');
+				if(url !== undefined && url.length > 1){
+					dynabox_content.removeAttr('title').find('.loading').show();
+					$.get(url, function(html){
+						dynabox_content.find('.loading').remove().end().append('<div class="content">' + html + '</div>');
+					});
+				}
+			}
+
+		};
+	}
+})(jQuery);
+
+
+
+jQuery(document).ready(
+		function() {
+			dynabox();
+		}
+);
