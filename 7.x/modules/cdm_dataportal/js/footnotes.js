292b1130b58c077643c59b67375e885f0c93fef5
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2013-01-18 16:54:39 +0000

    harmonizing footnotes.js

--- 7.x/modules/cdm_dataportal/js/footnotes.js
+++ 7.x/modules/cdm_dataportal/js/footnotes.js
@@ -1,34 +1,51 @@
-  jQuery(document).ready(function() {
-    
-    function getFootnoteClassName(object){
-      return '.'+jQuery(object).attr('href').substr(1);
-    }
-    
-    function getFootnoteKeyClassName(object){
-      return '.'+jQuery(object).attr('href').substr(1).replace(/-/gi, '-key-') + ' a';
-    }
-    
-    jQuery('span.footnote-key a').mouseover(function(e){
-      var fnClassName = getFootnoteClassName(this);
-      var fnKeyClassName = getFootnoteKeyClassName(this); 
-      jQuery('.footnote').css('background-color', 'transparent').css('background-color', 'transparent').removeClass('active');
-      jQuery('span.footnote-key a').css('background-color', 'transparent').css('background-color', 'transparent').removeClass('active');
-      jQuery(fnClassName).css('background-color', 'yellow');
-      jQuery(fnKeyClassName).css('background-color', 'yellow');
+/**
+ * Expected dom structure:
+ *  <li class="dynabox">
+      <div class="dynabox_label"><span class="label">Label Text</span>
+      <ul class="dynabox_content" title="{url-to-content}"><li> ...... </li></ul>
+    </li>
+ */
+
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function($){
+  this.footnotes = function() {
+
+      function getFootnoteClassName(object){
+        return '.'+$(object).attr('href').substr(1);
       }
-    ).mouseout(function(e){
-      var fnClassName = getFootnoteClassName(this);
-      var fnKeyClassName = getFootnoteKeyClassName(this); 
-      jQuery(fnClassName).not('.active').css('background-color', 'transparent');
-      jQuery(fnKeyClassName).not('.active').css('background-color', 'transparent');
+
+      function getFootnoteKeyClassName(object){
+        return '.'+$(object).attr('href').substr(1).replace(/-/gi, '-key-') + ' a';
       }
-    ).click(function(e){
-      var fnClassName = getFootnoteClassName(this);
-      var fnKeyClassName = getFootnoteKeyClassName(this); 
-      jQuery('.footnote').css('background-color', 'transparent').removeClass('active'); 
-      jQuery('span.footnote-key a').css('background-color', 'transparent').removeClass('active');
-      jQuery(fnClassName).css('background-color', 'yellow').addClass('active');
-      jQuery(fnKeyClassName).css('background-color', 'yellow').addClass('active');
-    });
-    
-  });
+
+      $('span.footnote-key a').mouseover(function(e){
+		var fnClassName = getFootnoteClassName(this);
+		var fnKeyClassName = getFootnoteKeyClassName(this);
+		$('.footnote').css('background-color', 'transparent').css('background-color', 'transparent').removeClass('active');
+		$('span.footnote-key a').css('background-color', 'transparent').css('background-color', 'transparent').removeClass('active');
+		$(fnClassName).css('background-color', 'yellow');
+		$(fnKeyClassName).css('background-color', 'yellow');
+		}
+      ).mouseout(function(e){
+        var fnClassName = getFootnoteClassName(this);
+        var fnKeyClassName = getFootnoteKeyClassName(this);
+        $(fnClassName).not('.active').css('background-color', 'transparent');
+        $(fnKeyClassName).not('.active').css('background-color', 'transparent');
+        }
+      ).click(function(e){
+        var fnClassName = getFootnoteClassName(this);
+        var fnKeyClassName = getFootnoteKeyClassName(this);
+        $('.footnote').css('background-color', 'transparent').removeClass('active');
+        $('span.footnote-key a').css('background-color', 'transparent').removeClass('active');
+        $(fnClassName).css('background-color', 'yellow').addClass('active');
+        $(fnKeyClassName).css('background-color', 'yellow').addClass('active');
+      });
+
+})(jQuery);
+
+jQuery(document).ready(
+    function() {
+      dynabox();
+    }
+);
