9c635db9c3cb5713dd89b954fe89d3080657d421
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2013-02-07 16:08:45 +0000

    show / hide gallery for specimens depending on open / close state of dynabox

--- 7.x/modules/cdm_dataportal/js/cdm_dynabox.js
+++ 7.x/modules/cdm_dataportal/js/cdm_dynabox.js
@@ -6,12 +6,22 @@
     </li>
  */
 
+// see also https://github.com/geetarista/jquery-plugin-template/blob/master/jquery.plugin-template.js
 
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
 ;(function($) {
-	this.dynabox = function(dynabox_id) {
 
+	// Default options for the plugin as a simple object
+	var defaults = {
+		 open_callback: function(){},
+         close_callback: function(){},
+	};
+
+	this.dynabox = function(dynabox_id, options) {
+
+		// Merge the options given by the user with the defaults
+	    this.options = $.extend({}, defaults, options);
 
 		$('.dynabox-' + dynabox_id + ' .label').click(function(event) {
 			loadContent(event);
@@ -27,19 +37,35 @@
 
 			if(dynabox_content.find('.content').length > 0) {
 				// content has already been loaded
-				dynabox_content.slideToggle("fast");
+				dynabox_content.slideToggle("fast", function(){
+						executeCallbacks(dynabox_content);
+					}
+				);
 			} else {
 				// no content so far, so load it
 				var url = dynabox_content.attr('title');
 				if(url !== undefined && url.length > 1){
-					dynabox_content.removeAttr('title').find('.loading').show();
+					dynabox_content.removeAttr('title').find('.loading').slideDown('fast',
+						function(){
+							executeCallbacks(dynabox_content);
+						}
+					);
 					$.get(url, function(html){
 						dynabox_content.find('.loading').remove().end().append('<div class="content">' + html + '</div>');
 					});
 				}
 			}
 
+
 		};
+
+		var executeCallbacks = function(dynabox_content) {
+			if (dynabox_content.css('display') == 'none'){
+				options.close_callback();
+			} else {
+				options.open_callback();
+			}
+		}
 	}
 })(jQuery);
 
