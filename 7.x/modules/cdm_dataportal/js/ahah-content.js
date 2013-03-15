b386ae48d45b5613cc30ff14e30eddd54cc7085d
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2013-03-15 19:56:09 +0000

    ahah-content, cdm_dynabox, footnotes as real jQuery plugins and fixing  #3272 (Footnote highlighting and ahah content are not workin in Dynabox) by listening to custom DOM event

--- 7.x/modules/cdm_dataportal/js/ahah-content.js
+++ 7.x/modules/cdm_dataportal/js/ahah-content.js
@@ -1,58 +1,81 @@
+// see also https://github.com/geetarista/jquery-plugin-template/blob/master/jquery.plugin-template.js
+
 /**
  * Expected dom structure:
  *  '<div class="ahah-content" rel="'.$cdm_proxy_url.'"><span class="loading">Loading ....</span></div>';
  */
-;(function($) {
+;(function($, document, window, undefined) {
 
-    this.ahahContentAutoAttach = function () {
 
-      // firebug console stub (avoids errors if firebug is not active)
-      if(typeof console === "undefined") {
-          console = { log: function() { } };
-      }
+    $.fn.ahahContent = function (eventSource) {
 
-      // register with all elements .ahah-content
-        $(".ahah-content").each(function(index, element) {
+    // firebug console stub (avoids errors if firebug is not active)
+    if(typeof console === "undefined") {
+        console = { log: function() { } };
+    }
+
+    var element;
+    if( eventSource !== undefined ){
+        // if ahahContent() has been called as event handler use the eventSource
+        element = $(eventSource);
+        console.log("ahahContent() as domEventHandler for " + element);
+    } else {
+        // otherwise use this jQuery object
+        element = $(this);
+        console.log("ahahContent() as jQuery function for " + element);
+    }
+
+
+      // register with all elements matching the css class selector '.ahah-content'
+      element.find(".ahah-content").each(function(index, element) {
+
           var ahahContent = $(element);
-          url = ahahContent.attr("rel");
-          console.log("ahah-content url:" + url);
+
+          var url = ahahContent.attr("data-cdm-ahah-url");
+          ahahContent.attr("data-cdm-ahah-url", '');
+          ahahContent.attr("data-cdm-ahah-url-loaded", url);
+
+          console.log("ahahContent() url:" + url);
           if(url !== undefined && url.length > 1){
-      // ahahContent.removeAttr('rel').find('.loading').css('display', 'block');
-            ahahContent.find('.loading').css('display', 'block');
-            $.get(url, function(html){
-              ahahContent.find('.loading').remove();
-              ahahContent.append(html);
-              });
-            }
-          });
-
-      // register with lightbox etc ...
-      $('body').bind('overflow', function(event){
-
-        var ahah_content_set = $(event.target).find('.ahah-content');
-        if(ahah_content_set !== undefined){
-          $(ahah_content_set).each(function(i){
-            var ahah_content = $(this);
-            var url = ahah_content.attr('rel');
-            console.log("ahah-content on 'overflow' url:" + url);
-            if(url !== undefined && url.length > 1){
-              ahah_content.removeAttr('rel').find('.loading').css('display', 'block');
+              ahahContent.find('.loading').css('display', 'block');
               $.get(url, function(html){
-                ahah_content.find('.loading').remove().end().append(html);
-                });
-            }
-          });
-
-        }
+                  ahahContent.find('.loading').remove().end().append(html);
+              });
+          }
       });
 
     }
-})(jQuery);
+})(jQuery, document, window);
 
 
 
 jQuery(document).ready(
     function() {
-        ahahContentAutoAttach();
+        //
+        jQuery("body").ahahContent();
+
+        // register events to detect lightbox opening
+        // for FireFox, Webkit and Opera
+        jQuery("body").bind('overflow', function(event){
+               jQuery('#jquery-lightbox').ahahContent();
+        });
+        // for Chrome and >=IE9(???)
+        //   - Chrome: this only works for the first image in a lighbox gallery FIXME (need lightbox with callback!)
+        jQuery("body").bind('overflowchanged', function(event){
+                window.setTimeout(
+                        function() {
+                            jQuery('#jquery-lightbox').ahahContent();
+                        },
+                        2000 // milli seconds
+                );
+        });
+
+        // register the ahahContent as domEventHandler
+        // see domEvents.js
+        //
+        // the for example dynabox will execute the callback after loading
+        // the content into the dynabox. By this it is for possible
+        // to have ahahContent content inside of dynaboxes
+        document.domEventHandlers.push(jQuery().ahahContent);
     }
 );
--- 7.x/modules/cdm_dataportal/js/cdm_dynabox.js
+++ 7.x/modules/cdm_dataportal/js/cdm_dynabox.js
@@ -12,60 +12,65 @@
 // scripts and/or other plugins which may not be closed properly.
 ;(function($) {
 
-	// Default options for the plugin as a simple object
-	var defaults = {
-		 open_callback: function(){},
+  // Default options for the plugin as a simple object
+  var defaults = {
+     open_callback: function(){},
          close_callback: function(){},
-	};
+  };
 
-	this.dynabox = function(dynabox_id, options) {
+  this.dynabox = function(dynabox_id, options) {
 
-		// Merge the options given by the user with the defaults
-	    this.options = $.extend({}, defaults, options);
+    // Merge the options given by the user with the defaults
+      this.options = $.extend({}, defaults, options);
 
-		$('.dynabox-' + dynabox_id + ' .label').click(function(event) {
-			loadContent(event);
-		});
+    $('.dynabox-' + dynabox_id + ' .label').click(function(event) {
+      loadContent(event);
+    });
 
-		$('.dynabox-' + dynabox_id).find('.dynabox-' + dynabox_id + '-content').click(function(event){event.stopPropagation();});
+    $('.dynabox-' + dynabox_id).find('.dynabox-' + dynabox_id + '-content').click(function(event){event.stopPropagation();});
 
-		//$('li.dynabox> span').click(function(event){event.stopPropagation();});
+    //$('li.dynabox> span').click(function(event){event.stopPropagation();});
 
-		var loadContent = function(event) {
-			event.preventDefault(); //Cancel the default action (navigation) of the click.
-			var dynabox_content = $(event.target).parent('.dynabox-' + dynabox_id).find('.dynabox-' + dynabox_id + '-content');
+    var loadContent = function(event) {
+      event.preventDefault(); //Cancel the default action (navigation) of the click.
+      var dynabox_content = $(event.target).parent('.dynabox-' + dynabox_id).find('.dynabox-' + dynabox_id + '-content');
 
-			if(dynabox_content.find('.content').length > 0) {
-				// content has already been loaded
-				dynabox_content.slideToggle("fast", function(){
-						executeCallbacks(dynabox_content);
-					}
-				);
-			} else {
-				// no content so far, so load it
-				var url = dynabox_content.attr('title');
-				if(url !== undefined && url.length > 1){
-					dynabox_content.removeAttr('title').find('.loading').slideDown('fast',
-						function(){
-							executeCallbacks(dynabox_content);
-						}
-					);
-					$.get(url, function(html){
-						dynabox_content.find('.loading').remove().end().append('<div class="content">' + html + '</div>');
-					});
-				}
-			}
+      if(dynabox_content.find('.content').length > 0) {
+        // content has already been loaded
+        dynabox_content.slideToggle("fast", function(){
+            toggleState(dynabox_content);
+          }
+        );
+      } else {
+        // no content so far, so load it
+        var url = dynabox_content.attr('title');
+        if(url !== undefined && url.length > 1){
+          dynabox_content.removeAttr('title').find('.loading').slideDown('fast',
+            function(){
+              toggleState(dynabox_content);
+            }
+          );
+          $.get(url, function(html){
+            dynabox_content.find('.loading').remove().end().append('<div class="content">' + html + '</div>').triggerElementsAdded()
+          });
+        }
+      }
 
 
-		};
+    };
 
-		var executeCallbacks = function(dynabox_content) {
-			if (dynabox_content.css('display') == 'none'){
-				options.close_callback();
-			} else {
-				options.open_callback();
-			}
-		}
-	}
+    /**
+     * toggles the closed/open state of the supplied dynabox
+     */
+    var toggleState = function(dynabox_content) {
+      if (dynabox_content.css('display') == 'none'){
+        options.close_callback();
+      } else {
+        options.open_callback();
+      }
+    }
+  }
+
+
 })(jQuery);
 
