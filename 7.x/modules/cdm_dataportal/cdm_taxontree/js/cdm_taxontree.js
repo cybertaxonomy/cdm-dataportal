0375638f71bb7ae42e55edd5dc0dd72159f35433
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2013-05-23 15:20:29 +0000

    major improvement in cdm_taxontree.js, fixes #3487 (long running java script in cdm_taxontree causing bad performance in all browsers and warnings in IE)

--- 7.x/modules/cdm_dataportal/cdm_taxontree/js/cdm_taxontree.js
+++ 7.x/modules/cdm_dataportal/cdm_taxontree/js/cdm_taxontree.js
@@ -1,60 +1,30 @@
-(function($) {
-  $.fn.cdm_taxontree = function(options) {
+// see also https://github.com/geetarista/jquery-plugin-template/blob/master/jquery.plugin-template.js
+
+/**
+ *
+ */
+;(function($, document, window, undefined) {
+
+    $.fn.cdm_taxontree = function(options) {
+
     var opts = $.extend({}, $.fn.cdm_taxontree.defaults, options);
-    return this.each(function() {
+
       /* ----------- magicbox ---------- */
       if (opts.magicbox) {
         $(this).cdm_taxontree_magicbox();
       }
+
       /* ----------- tree browser ---------- */
-      $(this).children('li').not('.invisible').click(
-          function(event) {
-            event.stopPropagation();
-            if ($(this).hasClass('collapsed')) {
-              var bindChildren = ($(this).find('ul').length == 0);
-              if (bindChildren) {
-                var url = $(this).attr('ref');
-                if (url != undefined) {
-                  $(this).removeAttr('ref');
-                  var parent_li = $(this);
-                  $(this).set_background_image('loading_subtree.gif');
-
-                  // Load DOM subtree and append it.
-                  $.get(url, function(html) {
-                    var tree_container = parent_li
-                        .parents('div.cdm_taxontree_container');
-                    parent_li.set_background_image('minus.png');
-                    if (opts.magicbox) {
-                      // Preserve scroll positions.
-                      var tmp_scroller_y_left = tree_container.children()
-                          .scrollTop();
-                      parent_li.append(html).find('ul').cdm_taxontree(options);
-
-                      // Resize parent container.
-                      tree_container.cdm_taxontree_container_resize();
-
-                      // Restore scroll positions.
-                      tree_container.children().scrollTop(tmp_scroller_y_left);
-                    } else {
-                      parent_li.append(html).find('ul').cdm_taxontree(options);
-                    }
-                  });
-                }
-              } else {
-                $(this).set_background_image('minus.png');
-              }
-              $(this).removeClass('collapsed').addClass('expanded').children(
-                  'ul').css('display', 'block');
-            } else if ($(this).hasClass('expanded')) {
-              $(this).removeClass('expanded').addClass('collapsed').children(
-                  'ul').css('display', 'none');
-              $(this).set_background_image('plus.png');
-            }
-          }); // END click().
-
-      $(this).children('li').children('a').click(function(event) {
-        event.stopPropagation();
-      });
+      $(this).delegate("li:not(.invisible)", "click", function(event) {
+              handle_taxon_node_click(event);
+          }
+      );
+      // Stop event propagation for links (unclear why this is nessecary,
+      // was this needed for the filter buttons?)
+      $(this).delegate("li a", "click", function(event) {
+              event.stopPropagation();
+          }
+      );
 
       /* ----------- widget ------------------- */
       if (opts.widget) {
@@ -75,14 +45,71 @@
         });
 
         bind_select_click(optionList, $(this), opts.multiselect);
-      }
-    });
+      };
 
+    /**
+     * handler function for clicks on the li elelements which
+     * represent CDM TaxonNodes. The click will load the
+     * nodes children via an AHAH call to the CdmdataPortal
+     * and expands the clicked node.
+     *
+     * @param event
+     *   The javascript event object of the click event
+     */
+    function handle_taxon_node_click(event){
+
+        event.stopPropagation();
+        var li = $(event.target);
+        if (li.hasClass('collapsed')) {
+          var bindChildren = (li.find('ul').length == 0);
+          if (bindChildren) {
+            var url = li.attr('ref');
+            if (url != undefined) {
+              li.removeAttr('ref');
+              var parent_li = li;
+              li.set_background_image('loading_subtree.gif');
+
+              // Load DOM subtree via AHAH and append it.
+              $.get(url, function(html) {
+                var tree_container = parent_li.parents('div.cdm_taxontree_container');
+                parent_li.set_background_image('minus.png');
+                if (opts.magicbox) {
+                  // Preserve scroll positions.
+                  var tmp_scroller_y_left = tree_container.children().scrollTop();
+
+                  parent_li.append(html).find('ul'); //.cdm_taxontree(options); // no longer required since we are using live() instead of click()
+
+                  // Resize parent container.
+                  tree_container.cdm_taxontree_container_resize();
+
+                  // Restore scroll positions.
+                  tree_container.children().scrollTop(tmp_scroller_y_left);
+                } else {
+                  parent_li.append(html).find('ul'); // .cdm_taxontree(options); // no longer required since we are using live() instead of click()
+                }
+              });
+            }
+          } else {
+            li.set_background_image('minus.png');
+          }
+          li.removeClass('collapsed').addClass('expanded').children(
+              'ul').css('display', 'block');
+        } else if (li.hasClass('expanded')) {
+          li.removeClass('expanded').addClass('collapsed').children(
+              'ul').css('display', 'none');
+          li.set_background_image('plus.png');
+        }
+    };
+
+    /**
+     *
+     */
     function bind_select_click(optionList, treelist, isMultiselect) {
       treelist.find('li .widget_select').click(
           function(event) {
             event.stopPropagation();
-            var value = $(this).attr('alt');
+            var li_widget_select = $(event.target);
+            var value = li_widget_select.attr('alt');
             if (optionList.children('[value=' + value + ']').length > 0) {
               // Remove from select.
               optionList.children('[value=' + value + ']').remove();
@@ -93,7 +120,7 @@
                 optionList.children().remove();
               }
               optionList.append('<option value="' + value + '">'
-                  + $(this).attr('title') + '</option>');
+                  + li_widget_select.attr('title') + '</option>');
 
               // Fix bug in IE.
               if (jQuery.browser['msie']) {
@@ -111,80 +138,60 @@
 
   }; // END cdm_taxontree()
 
-})(jQuery);
+})(jQuery, document, window);
 
 /**
- *
+ * helper function to set the background image as jQuery extension
  */
 jQuery.fn.set_background_image = function(imageFile) {
   var bg_image_tmp = jQuery(this).css('background-image');
-  var bg_image_new = bg_image_tmp.replace(/^(.*)(\/.*)(\))$/, '$1/' + imageFile
-      + '$3');
+  var bg_image_new = bg_image_tmp.replace(/^(.*)(\/.*)(\))$/, '$1/' + imageFile + '$3');
   if (jQuery.browser.mozilla) {
     // Special bug handling for mozilla: strip of last closing bracket.
     bg_image_new = bg_image_new.substr(0, bg_image_new.length - 1);
   }
   jQuery(this).css('background-image', bg_image_new);
-}
+};
+
+jQuery.fn.cdm_taxontree.defaults = { // Set up default options.
+        widget : false, // True = enable widget mode.
+        magicbox : false, // True = enable quirky magicbox.
+        element_name : 'widgetval',
+        multiselect : false // True = enable selection of multiple.
+};
+
+
+// ====================================================================================== //
 
 /**
- *
- */
-jQuery.fn.cdm_taxontree_container_resize = function() {
-  var current_w = $(this).parent().width();
-
-  // Determine max horizontal extent of any children.
-  var tree_list = $(this).find('.cdm_taxontree_scroller_y > ul');
-  var w = tree_list.css('position', 'absolute').outerWidth({
-    margin : true
-  });
-  tree_list.css('position', 'static');
-
-  // Other Browsers than Firefox.
-  if (jQuery.browser['msie']) {
-    if (jQuery.browser['version'].charAt(0) == '7') {
-      w = w + 17;
-    }
-    if (jQuery.browser['version'].charAt(0) <= '6') {
-      return;
-    }
-  }
-
-  if (current_w < w) {
-    jQuery(this).parent().width(w);
-    jQuery(this).children().width(w);
-  }
-  return jQuery(this).children().outerWidth();
-}
-
-/**
- *
- */
-jQuery.fn.cdm_taxontree_container_debug_size = function(msg) {
-  var out = msg + '<br />    scoll_x: ' + $(this).parent().width()
-      + '<br />    container: ' + $(this).width() + '<br />    scoll_y: '
-      + $(this).children().width() + '<br />    ul: '
-      + $(this).find('.cdm_taxontree_scroller_y > ul').width() + '<br />';
-  jQuery('#DEBUG_JS').append(out);
-}
-
-/**
- *
+ * jQuery function wich implements the Magicbox behavior for the taxontree.
+ * A Magicbox the container of the taxon tree will extend on mouse over
+ * events in order to show the entrys without truncation through cropping.
  */
 jQuery.fn.cdm_taxontree_magicbox = function() {
   // Exclude IE6 and lower versions.
   if (!(jQuery.browser['msie'] && jQuery.browser['version'].charAt(0) < '7')) {
 
     var container = $(this).parent().parent('div.cdm_taxontree_container');
-    if (container[0] == undefined) {
-      return;
+    if (container[0] != undefined) {
+        container.hover(
+            function(event) {
+                handle_mouseOver(event);
+            },
+            function(event) {
+                handle_mouseOut(event);
+            }
+        );
     }
+  } // END exclude IE6
 
-    container.hover(
-    // --- mouseOver ---- //
-    function() {
-      var scroller_x = $(this).parent();
-      var scroller_y = $(this).children('.cdm_taxontree_scroller_y');
+  /**
+   * expands the box on mouse over events the
+   */
+  function handle_mouseOver(event){
+      var taxontree_container = $(event.target);
+      var scroller_x = taxontree_container.parent();
+      var scroller_y = taxontree_container.children('.cdm_taxontree_scroller_y');
       var container = scroller_x.parent();
 
       var h = parseFloat(scroller_x.height());
@@ -192,34 +199,34 @@ jQuery.fn.cdm_taxontree_magicbox = function() {
       var border_color = scroller_x.css('border-top-color');
 
       // Store scroll_left of scroller_x so that it can be restored on mouseOut.
-      scroller_x
-          .append('<div class="_scrollLeft" style="display: none;" title="'
+      scroller_x.append('<div class="_scrollLeft" style="display: none;" title="'
               + scroller_x.scrollLeft() + '"></div>');
 
-      var newWidth = $(this).cdm_taxontree_container_resize();
+      var newWidth = cdm_taxontree_container_resize(taxontree_container);
 
+      var shift_left = '0';
       if (scroller_x.hasClass('expand-left')) {
-        var shift_left = container.outerWidth({
-          margin : true
-        }) - newWidth;
-      } else {
-        var shift_left = '0';
+        shift_left = container.outerWidth({margin : true}) - newWidth;
       }
 
-      scroller_y.css('overflow-y', 'auto').css('border-color', border_color)
+      scroller_y.css('overflow-y', 'auto')
+          .css('border-color', border_color)
           .scrollTop(scroll_top);
-      scroller_x.css('overflow-y', 'visible').css('overflow-x', 'visible').css(
-          'margin-left', shift_left).css('border-color', 'transparent').height(
-          h);
-    }
-    // ----------------- //
-    ,
-    // --- mouseOut ---- //
-    function() {
-      // return; 
-      var container = jQuery(this);
-      var scroller_x = jQuery(this).parent('.cdm_taxontree_scroller_x');
-      var scroller_y = container.children('.cdm_taxontree_scroller_y');
+      scroller_x.css('overflow-y', 'visible')
+          .css('overflow-x', 'visible')
+          .css('margin-left', shift_left)
+          .css('border-color', 'transparent')
+          .height(h);
+  };
+
+  /**
+   * Restores the original appearance on mouse out events
+   */
+  function handle_mouseOut(event){
+
+      var taxontree_container = $(event.target);
+      var scroller_x = taxontree_container.parent('.cdm_taxontree_scroller_x');
+      var scroller_y = taxontree_container.children('.cdm_taxontree_scroller_y');
       var border_color = scroller_y.css('border-top-color');
 
       var scroll_top = scroller_y.scrollTop();
@@ -228,27 +235,52 @@ jQuery.fn.cdm_taxontree_magicbox = function() {
       scroller_x.css('overflow-y', 'auto').css('margin-left', '0').css(
           'border-color', border_color).width('auto').scrollTop(scroll_top);
 
-      // Restore scroll_left of scroller_x. 
+      // Restore scroll_left of scroller_x.
       var scrollLeft = scroller_x.children('._scrollLeft').attr('title');
-      scroller_x.scrollLeft(scrollLeft)
+      scroller_x.scrollLeft(scrollLeft);
       scroller_x.children('._scrollLeft').remove();
-    }
-    // ------------------ //
-    );
-  }
-  // END exclude IE6    
-}
+  };
 
-jQuery.fn.cdm_taxontree.defaults = { // Set up default options.
-  widget : false, // True = enable widget mode.
-  magicbox : false, // True = enable quirky magicbox.
-  element_name : 'widgetval',
-  multiselect : false // True = enable selection of multiple.
+  /**
+   * Resizes the taxontree_container and returns the new outerWidth
+   */
+  function taxontree_container_resize(taxontree_container) {
+    var current_w = taxontree_container.parent().width();
+
+    // Determine max horizontal extent of any children.
+    var tree_list = taxontree_container.find('.cdm_taxontree_scroller_y > ul');
+    var w = tree_list.css('position', 'absolute').outerWidth({
+      margin : true
+    });
+    tree_list.css('position', 'static');
+
+    // Other Browsers than Firefox.
+    if (jQuery.browser['msie']) {
+      if (jQuery.browser['version'].charAt(0) == '7') {
+        w = w + 17;
+      }
+      if (jQuery.browser['version'].charAt(0) <= '6') {
+        return;
+      }
+    }
+
+    if (current_w < w) {
+        taxontree_container.parent().width(w);
+        taxontree_container.children().width(w);
+    }
+    return taxontree_container.children().outerWidth();
+  };
+
+  /**
+   * Debug function, currently unused
+   */
+  function cdm_taxontree_container_debug_size(taxontree_container, msg) {
+      var out = msg + '<br />    scoll_x: ' + taxontree_container.parent().width()
+          + '<br />    container: ' + taxontree_container.width() + '<br />    scoll_y: '
+          + taxontree_container.children().width() + '<br />    ul: '
+          + taxontree_container.find('.cdm_taxontree_scroller_y > ul').width() + '<br />';
+      jQuery('#DEBUG_JS').append(out);
+  };
 };
 
-/* ========================== auto activate ========================== */
-/*
-jQuery(document).ready(function() {
-  jQuery('ul.cdm_taxontree').cdm_taxontree();
-});
- */
+//====================================================================================== //
