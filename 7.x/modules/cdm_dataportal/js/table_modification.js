64f3df63c7040a8e954442fda216130cbae6c4f7
Patric Plitzner <p.plitzner@bgbm.org>
2014-07-31 13:25:36 +0000

    - some row toggle updates using the correct ids

--- 7.x/modules/cdm_dataportal/js/table_modification.js
+++ 7.x/modules/cdm_dataportal/js/table_modification.js
@@ -1,9 +1,21 @@
 /**
+ * Adds the functionality to toggle (hide/show) every second row by clicking on the first.
+ * All first rows must belong to the class "summary_row"
+ * and the second rows to the class "detail_row".
  *
+ * @param selector the CSS selector for the table on which this functionality should be applied
  */
 function addRowToggle(selector) {
 
+    //hide all detail rows
+    //jQuery(selector)[0].getElementsByClassName("detail_row").forEach(hide());
+    //var detailRows = jQuery(selector)[0].getElementsByClassName("detail_row");
+    //for(var i=0; i < detailRows.length; i++){
+    //    detailRows[i].hide();
+    //}
+
     jQuery(selector).click(function(event){
-        jQuery(selector).hide();
+        jQuery("#derivate_details1").toggle(1000);
+        //event.currentTarget.getElementsByClassName("detail_row").toggle(1000);
     })
 }
