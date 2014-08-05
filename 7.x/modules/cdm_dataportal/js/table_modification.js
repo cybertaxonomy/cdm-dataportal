29f3440ed4bda03576f565b48da56c14720d8035
Patric Plitzner <p.plitzner@bgbm.org>
2014-08-05 08:13:37 +0000

    - added more parameters for derivate hierarchy to PHP  - cleaned up code  - documentation

--- 7.x/modules/cdm_dataportal/js/table_modification.js
+++ 7.x/modules/cdm_dataportal/js/table_modification.js
@@ -7,15 +7,30 @@
  */
 function addRowToggle(selector) {
 
-    //hide all detail rows
-    jQuery(selector + " .detail_row").hide();
+    //hide all detail rows and color them
+    jQuery(selector + " .detail_row").hide().css("background","#F9F9F9");
 
-    jQuery(selector + " .summary_row").click(function(event){//register click on every summary row
-        jQuery(event.target).parent(".summary_row").next().toggle(500);//toggle detail row when clicking on corresponding summary row
-    }).hover(function(event){//register mouse over on every summary row
-        jQuery(event.target).parent(".summary_row").css("background","#FFCC00");
-    },function(event){//register mouse over on every summary row
-        jQuery(event.target).parent(".summary_row").css("background","");
-    }).css('cursor', 'hand').css('cursor', 'pointer');//show mouse cursor as a link
-
+    //register click on every summary row
+    jQuery(selector + " .summary_row").click(
+        function(event){
+            jQuery(event.target).parent(".summary_row").next().toggle(500);//toggle detail row when clicking on corresponding summary row
+        })
+        //color summary row when hovering over it
+        .mouseenter(
+        function(event){
+            jQuery(event.target).parent(".summary_row").css("background","#FFCC00");
+        })
+        .mouseleave(
+        function(event){
+            jQuery(event.target).parent(".summary_row").css("background","");
+        })
+        //show mouse cursor as a link
+        .css('cursor', 'hand').css('cursor', 'pointer');
 }
+//.hover(//register mouse hover on every summary row
+//    function(event){
+//        jQuery(event.target).parent(".summary_row").css("background","#FFCC00");
+//    }
+//    ,function(event){
+//        jQuery(event.target).parent(".summary_row").css("background","");
+//    })
