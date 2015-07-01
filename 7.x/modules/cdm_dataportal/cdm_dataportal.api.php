f78fda8182753b339d083741f307cbac37d538a7
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2015-07-01 11:50:26 +0200

    code documentation cosmetics

--- 7.x/modules/cdm_dataportal/cdm_dataportal.api.php
+++ 7.x/modules/cdm_dataportal/cdm_dataportal.api.php
@@ -31,7 +31,13 @@ function hook_cdm_feature_node_blocks_alter(&$block_list, $taxon = NULL) {
 
     if ($taxon) {
         // taxon is only given if called from within the taxon general page part
-        $numberOfChildren = count(cdm_ws_get(CDM_WS_PORTAL_TAXONOMY_CHILDNODES_OF_TAXON, array (get_taxonomictree_uuid_selected(), $taxon->uuid)));
+        $numberOfChildren = count(
+          cdm_ws_get(CDM_WS_PORTAL_TAXONOMY_CHILDNODES_OF_TAXON,
+              array (get_taxonomictree_uuid_selected(),
+              $taxon->uuid
+              )
+          )
+        );
         $subRank = 'sub taxa';
         if ($taxon->name->rank->titleCache == "Genus") {
             $subRank = "species";
@@ -46,7 +52,11 @@ function hook_cdm_feature_node_blocks_alter(&$block_list, $taxon = NULL) {
         if ($numberOfChildren > 0) {
             $block = feature_block('Number of Taxa');
             // FIXME use compose_cdm_feature_block_elements() in next line
-            $block->content = array(markup_to_render_array('<ul class="feature-block-elements"><li>' . $numberOfChildren . " " . $subRank . '</li></ul>'));
+            $block->content = array(markup_to_render_array(
+                '<ul class="feature-block-elements"><li>'
+                . $numberOfChildren . " " . $subRank
+                . '</li></ul>')
+            );
             array_unshift($block_list, $block);
             cdm_toc_list_add_item('Number of Taxa', 'number-of-taxa', NULL, TRUE);
         }
