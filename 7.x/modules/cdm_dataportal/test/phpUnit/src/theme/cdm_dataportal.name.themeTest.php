c8c879d86e7f9cd70a311c8b83f5a61515dc3e2e
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2015-09-10 17:39:45 +0200

    #5182 some usages of theme_cdm_taxonName have been not adapded to the new function render_taxon_or_name - fixing this

--- 7.x/modules/cdm_dataportal/test/phpUnit/src/theme/cdm_dataportal.name.themeTest.php
+++ 7.x/modules/cdm_dataportal/test/phpUnit/src/theme/cdm_dataportal.name.themeTest.php
@@ -2,11 +2,11 @@
 
 class NameThemeTests extends PHPUnit_Framework_TestCase {
 
-  function test_theme_cdm_taxonName() {
+  function test_render_taxon_or_name() {
     $taxon1_file = "theme/cdm_dataportal.name.themeTest-taxon1.json";
     $taxon = TestUtils::load_from_json_resource($taxon1_file);
     $this->assertEquals($taxon->uuid, "0ae4f7ab-f482-482c-ba57-cf0e4389a417");
-    $out = theme_cdm_taxonName($taxon->name);
+    $out = render_taxon_or_name($taxon->name);
     TestUtils::stderr($out);
   }
 
