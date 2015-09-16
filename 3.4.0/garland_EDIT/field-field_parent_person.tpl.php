5c4899e7ef03eb8af3ca5848c9d2232e1abc142d
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2015-09-16 13:54:56 +0000

    removing old release tag which has been replaced by tags/drupal/3.4.0

--- 3.4.0/garland_EDIT/field-field_parent_person.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    foreach ($items as $item) { 
-      $user_profile = node_load($item['nid']);
-      $output = l($user_profile->title, 'user/'.$user_profile->uid); 
-      print $output ?><br/>
-<?php } ?>
