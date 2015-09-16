a273565f86cfe703d2dd3c76d85dce121a7632c7
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2015-09-16 13:54:34 +0000

    removing old release tag which has been replaced by tags/drupal/3.3.2

--- 3.3.2/palmweb_2/print.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    $(document).ready(function () { 
-        $('#print-button').append('<a href="#print"><img title="Print this page" alt="Print this page " src="/files/cdm_dataportal/print_icon.gif "> Print this page</a>');
-        $('#print-button').click(function () {
-            window.print();
-        });
-    });
-})(jQuery);
