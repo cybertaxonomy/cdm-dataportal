3162a0d0f403ae4264bfddfb800ae0c323de6d83
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2016-03-23 11:28:00 +0100

    updating  test to changed/improved intra synonym group ordering

--- src/test/java/eu/etaxonomy/dataportal/selenium/tests/cichorieae/Crepis_tectorum_SynonymyTest.java
+++ src/test/java/eu/etaxonomy/dataportal/selenium/tests/cichorieae/Crepis_tectorum_SynonymyTest.java
@@ -33,12 +33,26 @@ public class Crepis_tectorum_SynonymyTest extends CdmDataPortalTestBase{
     static UUID crepis_tectorum_Uuid = UUID.fromString("c62dff09-3f04-4f05-9aac-904d51ac1b77");
 
     @Test
-    public void crepis_oenipontana() throws MalformedURLException {
+    public void crepis_tectorum_synonyms() throws MalformedURLException {
         TaxonSynonymyPage p = new TaxonSynonymyPage(driver, getContext(), crepis_tectorum_Uuid);
-        assertEquals(getContext().prepareTitle("Crepis tectorum"), p.getTitle());
+        assertEquals(getContext().prepareTitle("Crepis tectorum"), driver.getTitle());
+
         assertEquals("Crepis tectorum L., Sp. Pl.: 807. 1753", p.getAcceptedNameText());
-        assertEquals("≡ Crepis varia Moench, Methodus: 534. 1794, nom. illeg.", p.getHomotypicalGroupSynonymName(2));
+
+        // HomotypicalGroupSynonyms
+        assertEquals("≡ Hedypnois tectorum (L.) Huds., Fl. Angl., ed. 2: 341. 1778", p.getHomotypicalGroupSynonymName(1));
+        assertEquals("≡ Hieracium tectorum (L.) Karsch, Fl. Westfalen: 331. 1853", p.getHomotypicalGroupSynonymName(2));
+        assertEquals("≡ Hieracioides tectorum (L.) Kuntze, Revis. Gen. Pl. 1: 346. 1891", p.getHomotypicalGroupSynonymName(3));
+        assertEquals("≡ Crepis varia Moench, Methodus: 534. 1794, nom. illeg.", p.getHomotypicalGroupSynonymName(4));
+        assertEquals("≡ Crepis muralis Salisb., Prodr. Stirp. Chap. Allerton: 182. 1796, nom. illeg.", p.getHomotypicalGroupSynonymName(5));
+        assertEquals("≡ Crepis tectoria Dulac, Fl. Hautes-Pyrénées: 491. 1867, nom. illeg.", p.getHomotypicalGroupSynonymName(6));
+
+        // 1st HeterotypicalGroup
+        assertEquals("= Crepis tectorum var. segetalis Roth, Tent. Fl. Germ. 2: 254. 17931", p.getHeterotypicalGroupSynonymName(1, 1));
         assertEquals("≡ Crepis stricta Schultz, Prodr. Fl. Starg. Suppl.: 41. 1819 [non Crepis stricta Scop. 1772]", p.getHeterotypicalGroupSynonymName(1, 2));
+        assertEquals("≡ Crepis tectorum var. stricta E. Mey. ex Bisch., Beitr. Fl. Deutschl.: 274. 1851, nom. illeg.", p.getHeterotypicalGroupSynonymName(1, 3));
+        assertEquals("≡ Crepis tectorum var. minima Schur, Enum. Pl. Transsilv.: 376. 1866, nom. illeg.", p.getHeterotypicalGroupSynonymName(1, 4));
+
         assertEquals("= Crepis lanceolata Kit. [non Crepis lanceolata Sch. Bip. 1854]", p.getHeterotypicalGroupSynonymName(9, 1));
     }
 
