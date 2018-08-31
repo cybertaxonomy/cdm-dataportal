41fb11515c84cc7f7088365db413106689d867c7
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2018-08-31 17:15:51 +0200

    more tests for misapplied names

---
+++ src/main/java/eu/etaxonomy/dataportal/StringConstants.java
@@ -0,0 +1,23 @@
+/**
+* Copyright (C) 2018 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.dataportal;
+
+/**
+ * @author a.kohlbecker
+ * @since Aug 31, 2018
+ *
+ */
+public class StringConstants {
+
+    /**
+     * SMALL QUESTION MARK + NARROW NO-BREAK
+     */
+    public static final String DOUBTFULMARKER_SPACE = "/uFE56/u8239";
+
+}
--- src/test/java/eu/etaxonomy/dataportal/selenium/tests/reference/MisappliedNamesTest.java
+++ src/test/java/eu/etaxonomy/dataportal/selenium/tests/reference/MisappliedNamesTest.java
@@ -18,12 +18,19 @@ import org.openqa.selenium.WebElement;
 
 import eu.etaxonomy.dataportal.DataPortalSite;
 import eu.etaxonomy.dataportal.ElementUtils;
+import eu.etaxonomy.dataportal.StringConstants;
 import eu.etaxonomy.dataportal.elements.BaseElement;
 import eu.etaxonomy.dataportal.junit.CdmDataPortalTestBase;
 import eu.etaxonomy.dataportal.junit.DataPortalContextSuite.DataPortalContexts;
 import eu.etaxonomy.dataportal.pages.TaxonSynonymyPage;
 
 /**
+ * Issues to be covered by this TestClass:
+ *
+ * #5676
+ * #5647 - OK
+ * #5492 - OK
+ *
  * @author a.kohlbecker
  *
  */
@@ -33,16 +40,23 @@ public class MisappliedNamesTest extends CdmDataPortalTestBase{
 
     static final UUID miconia_cubacinerea_Uuid = UUID.fromString("c6716cee-2039-4ba8-a239-4b1b353f9c84");
 
+
+    static final UUID trichocentrum_undulatum_Uuid = UUID.fromString("7e86b2a4-ba71-4494-b544-ae5656e02ed2");
+
     @Before
     public void setUp() throws Exception {
         driver.get(getContext().getBaseUri().toString());
     }
 
     /**
-     * Test for correct sensu representation of misapplied names, see #5676
+     * Test for correct sensu representation of misapplied names, see #5676 and #5647
+     *
+     * https://dev.e-taxonomy.eu/redmine/issues/5647
+     *
+     * NOTE: Species solaris has no authorship!!
      */
     @Test
-    public void testTitleAndTabs() throws MalformedURLException {
+    public void tesIssue5647() throws MalformedURLException {
 
         TaxonSynonymyPage p = new TaxonSynonymyPage(driver, getContext(), miconia_cubacinerea_Uuid);
 
@@ -52,6 +66,30 @@ public class MisappliedNamesTest extends CdmDataPortalTestBase{
         List<BaseElement> footnotes = ElementUtils.findFootNotes(p.getTaxonRelationships());
         assertEquals(1, footnotes.size());
         assertEquals("1. Lem, New Species in the solar system", footnotes.get(0).getText());
+
+    }
+
+
+    /**
+     * https://dev.e-taxonomy.eu/redmine/issues/5492
+     */
+    @Test
+    public void testIssue5492() throws MalformedURLException {
+
+        TaxonSynonymyPage p = new TaxonSynonymyPage(driver, getContext(), trichocentrum_undulatum_Uuid);
+
+        WebElement misappliedName1 = p.getMisappliedName(1);
+        assertNotNull(misappliedName1);
+        assertEquals("–\n\"Oncidium guttatum\" auct. sensu Greuter, W. & Rankin Rodríguez, R1", misappliedName1.getText());
+
+        WebElement misappliedName2 = p.getMisappliedName(2);
+        assertNotNull(misappliedName2);
+        assertEquals("–\n" + StringConstants.DOUBTFULMARKER_SPACE +"\"Oncidium carthaginense\" auct. sensu Greuter, W. & Rankin Rodríguez, R1", misappliedName2.getText());
+
+
+        List<BaseElement> footnotes = ElementUtils.findFootNotes(p.getTaxonRelationships());
+        assertEquals(1, footnotes.size());
+        assertEquals("1. Greuter, W. & Rankin Rodríguez, R, Plantas vasculares de Cuba: inventario preliminar. Tercera edición, actualizada. Vascular plants of Cuba: a preliminary checklist. Third updated edition.", footnotes.get(0).getText());
 
     }
 
