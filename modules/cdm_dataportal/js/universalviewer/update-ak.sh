dde81daa4996046de15515768a113ff78d945e04
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2020-03-09 12:32:54 +0100

    ref #5083 adding developer utility script

---
+++ modules/cdm_dataportal/js/universalviewer/update-ak.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf ~/workspaces/cdm/cdm-dataportal/modules/cdm_dataportal/js/universalviewer/uv
+cp -a ~/workspaces/libs/universalviewer/dist ~/workspaces/cdm/cdm-dataportal/modules/cdm_dataportal/js/universalviewer/uv
+
