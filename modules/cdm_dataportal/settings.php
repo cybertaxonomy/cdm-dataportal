af28dbeda88a6c503af4147623dcc7d3c547437c
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2020-07-24 07:45:38 +0200

    more max map zoomlevel values

--- modules/cdm_dataportal/settings.php
+++ modules/cdm_dataportal/settings.php
@@ -3159,7 +3159,7 @@ function cdm_settings_geo($form, &$form_state) {
     '#type' => 'select',
     '#title' => 'Max zoom level',
     '#default_value' => $map_distribution['maxZoom'],
-    '#options' => array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+    '#options' => array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
   );
 
   $form[CDM_MAP_DISTRIBUTION]['show_labels'] = array(
