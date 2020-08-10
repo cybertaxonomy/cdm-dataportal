20ceff0b10d1079c0463112a482bbe5c51cc54cc
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2020-08-10 14:32:47 +0200

    fix #9181 DrushExecutor with option to work remotely

--- src/main/java/eu/etaxonomy/drush/DrushExecuter.java
+++ src/main/java/eu/etaxonomy/drush/DrushExecuter.java
@@ -26,6 +26,15 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 /**
+ * Java executor for drush (https://www.drush.org/).
+ *
+ * <h3>Local usage:</h3> Set the {@link #setDrupalRoot(File) drupal root folder}
+ * and the {@link #setSiteURI(URI) site uri}.
+ *
+ * <h3>Remote usage:</h3> In addition to the above properties you can also
+ * define an {@link #setSshHost(String) ssh host} and optionally the according
+ * {@link #setSshUser(String) ssh user} to execute drush on a remote machine.
+ *
  * @author a.kohlbecker
  * @since Jul 31, 2020
  */
@@ -33,33 +42,21 @@ public class DrushExecuter {
 
     public static final Logger logger = Logger.getLogger(DrushExecuter.class);
 
+    private URI siteURI = null;
 
-    public URI getSiteURI() {
-        return siteURI;
-    }
+    private File drupalRoot = null;
 
-    public void setSiteURI(URI siteURI) {
-        this.siteURI = siteURI;
-    }
+    private String sshUser = null;
 
-    public File getDrupalRoot() {
-        return drupalRoot;
-    }
-
-    public void setDrupalRoot(File drupalRoot) {
-        this.drupalRoot = drupalRoot;
-    }
-
-    public URI siteURI = null;
-
-    public File drupalRoot = null;
+    private String sshHost = null;
 
     public DrushExecuter() throws IOException, InterruptedException {
-       findDrushCommand();
+        findDrushCommand();
     }
 
     /**
-     * List indexes returned from <code>DrushExecuter.execute(DrushCommand cmd, String... value)</code>:
+     * List indexes returned from
+     * <code>DrushExecuter.execute(DrushCommand cmd, String... value)</code>:
      *
      * <ol>
      * <li>major</li>
@@ -67,15 +64,16 @@ public class DrushExecuter {
      * <li>patch</li>
      * <ol>
      */
-    public static DrushCommand version = new DrushCommand(Arrays.asList("--version"), "Drush Version\\s+:\\s+(?<major>\\d+)\\.(?<minor>\\d+)\\.(?<patch>\\d+)", null);
-
+    public static DrushCommand version = new DrushCommand(Arrays.asList("--version"),
+            "Drush Version\\s+:\\s+(?<major>\\d+)\\.(?<minor>\\d+)\\.(?<patch>\\d+)", null);
 
     public static DrushCommand help = new DrushCommand(Arrays.asList("help"), null, null);
 
     public static DrushCommand coreStatus = new DrushCommand(Arrays.asList("core-status"), null, null);
 
     /**
-     * List indexes returned from <code>DrushExecuter.execute(DrushCommand cmd, String... value)</code>:
+     * List indexes returned from
+     * <code>DrushExecuter.execute(DrushCommand cmd, String... value)</code>:
      * Multiple matches are possible:
      * <ol>
      * <li>value</li>
@@ -83,55 +81,72 @@ public class DrushExecuter {
      * <li>value</li>
      * <ol>
      */
-    public static DrushCommand variableGet = new DrushCommand(Arrays.asList("vget", "%s"), ".*:\\s+'(?<value>.*)'", null);
+    public static DrushCommand variableGet = new DrushCommand(Arrays.asList("vget", "%s"), ".*:\\s+'(?<value>.*)'",
+            null);
 
     /**
-     * List indexes returned from <code>DrushExecuter.execute(DrushCommand cmd, String... value)</code>:
+     * List indexes returned from
+     * <code>DrushExecuter.execute(DrushCommand cmd, String... value)</code>:
      * Multiple matches are possible:
      * <ol>
      * <li>value</li>
      * <li>status</li>
      * <ol>
      */
-    public static DrushCommand variableSet = new DrushCommand(Arrays.asList("--yes", "vset", "%s", "%s"), null,  "[^\\\"]*\\\"(?<value>.*)\\\"\\.\\s+\\[(?<status>\\w+)\\]");
+    public static DrushCommand variableSet = new DrushCommand(Arrays.asList("--yes", "vset", "%s", "%s"), null,
+            "[^\\\"]*\\\"(?<value>.*)\\\"\\.\\s+\\[(?<status>\\w+)\\]");
 
     /**
-     * @throws IOException if an I/O error occurs in the ProcessBuilder
-     * @throws InterruptedException if the Process was interrupted
+     * @throws IOException
+     *             if an I/O error occurs in the ProcessBuilder
+     * @throws InterruptedException
+     *             if the Process was interrupted
      */
     private void findDrushCommand() throws IOException, InterruptedException {
 
-        if(SystemUtils.IS_OS_WINDOWS) {
+        if (SystemUtils.IS_OS_WINDOWS) {
             throw new RuntimeException("not yet implmented for Windows");
         }
 
         List<String> matches = execute(version);
         assert !matches.get(0).isEmpty() : "No suitable drush command found in the system";
         String majorVersion = matches.get(0);
-        if(Integer.valueOf(majorVersion) < 8) {
+        if (Integer.valueOf(majorVersion) < 8) {
             throw new RuntimeException("drush version >= 8 required");
         }
 
     }
 
     /**
-     * @throws IOException if an I/O error occurs in the ProcessBuilder
-     * @throws InterruptedException if the Process was interrupted
+     * @throws IOException
+     *             if an I/O error occurs in the ProcessBuilder
+     * @throws InterruptedException
+     *             if the Process was interrupted
      */
-    public List<String> execute(DrushCommand cmd, String ...value ) throws IOException, InterruptedException {
+    public List<String> execute(DrushCommand cmd, String... value) throws IOException, InterruptedException {
 
         List<String> executableWithArgs = new ArrayList<>();
+
+        if (sshHost != null) {
+            executableWithArgs.add("ssh");
+            String userHostArg = sshHost;
+            if (sshUser != null) {
+                userHostArg = sshUser + "@" + sshHost;
+            }
+            executableWithArgs.add(userHostArg);
+        }
+
         executableWithArgs.add("drush");
 
-        if(drupalRoot != null) {
-            executableWithArgs.add("--root="+drupalRoot.toString());
+        if (drupalRoot != null) {
+            executableWithArgs.add("--root=" + drupalRoot.toString());
         }
-        if(siteURI != null) {
+        if (siteURI != null) {
             executableWithArgs.add("--uri=" + siteURI.toString());
         }
         int commandSubstitutions = 0;
-        for(String arg  : cmd.args) {
-            if(arg.contains("%s")) {
+        for (String arg : cmd.args) {
+            if (arg.contains("%s")) {
                 executableWithArgs.add(String.format(arg, value[commandSubstitutions]));
                 commandSubstitutions++;
             } else {
@@ -146,13 +161,13 @@ public class DrushExecuter {
         Process process = pb.start();
         int exitCode = process.waitFor();
 
-        if(exitCode == 0) {
+        if (exitCode == 0) {
             String out = readExecutionResponse(matches, process.getInputStream(), cmd.outRegex);
             String error = readExecutionResponse(matches, process.getErrorStream(), cmd.errRegex);
-            if(out != null && !out.isEmpty()) {
+            if (out != null && !out.isEmpty()) {
                 logger.error(error);
             }
-            if(error != null && !error.isEmpty()) {
+            if (error != null && !error.isEmpty()) {
                 logger.error(error);
             }
         } else {
@@ -163,22 +178,22 @@ public class DrushExecuter {
 
     protected String readExecutionResponse(List<String> matches, InputStream stream, Pattern regex) throws IOException {
         String out;
-        if(regex != null) {
+        if (regex != null) {
             Scanner scanner = new Scanner(stream);
-            while(true) {
+            while (true) {
                 out = scanner.findWithinHorizon(regex, 0);
-                if(out == null) {
+                if (out == null) {
                     break;
                 }
-                if(out != null) {
+                if (out != null) {
                     Matcher m = regex.matcher(out);
                     int patternMatchCount = 0;
-                    while(m.find()) {
+                    while (m.find()) {
                         patternMatchCount++;
-                        if(m.groupCount() > 0) {
-                            for(int g = 1 ; g <= m.groupCount(); g++) {
+                        if (m.groupCount() > 0) {
+                            for (int g = 1; g <= m.groupCount(); g++) {
                                 matches.add(m.group(g));
-                                logger.debug("match[" + patternMatchCount + "."+ g + "]: " + m.group(g));
+                                logger.debug("match[" + patternMatchCount + "." + g + "]: " + m.group(g));
                             }
                         } else {
                             matches.add(m.group(0));
@@ -197,7 +212,6 @@ public class DrushExecuter {
 
     }
 
-
     public static class DrushCommand {
 
         Pattern outRegex;
@@ -206,27 +220,80 @@ public class DrushExecuter {
 
         public DrushCommand(List<String> args, String outRegex, String errRegex) {
             this.args = args;
-            if(outRegex != null) {
+            if (outRegex != null) {
                 this.outRegex = Pattern.compile(outRegex, Pattern.MULTILINE);
             }
-            if(errRegex != null) {
+            if (errRegex != null) {
                 this.errRegex = Pattern.compile(errRegex, Pattern.MULTILINE);
             }
         }
     }
 
-    public static void main(String[] args) throws URISyntaxException  {
+    /**
+     * These tests have not been implemented as jUnit tests since the execution
+     * it too much dependent from the local environment. Once the
+     * <code>DrushExecuter</code> is being used in the selenium test suite will
+     * be tested implicitly anyway.
+     */
+    public static void main(String[] args) throws URISyntaxException {
         DrushExecuter.logger.setLevel(Level.DEBUG);
         try {
             DrushExecuter dex = new DrushExecuter();
             dex.setDrupalRoot(new File("/home/andreas/workspaces/www/drupal-7"));
             dex.setSiteURI(new URI("http://edit.test/d7/caryophyllales/"));
-//            dex.execute(coreStatus);
-//            dex.execute(help);
-//            dex.execute(variableGet, "cdm_webservice_url");
-            dex.execute(variableSet, "cdm_webservice_url", "http://api.cybertaxonomy.org/cyprus/");
+            dex.execute(coreStatus);
+            dex.execute(help);
+            List<String> results = dex.execute(variableSet, "cdm_webservice_url",
+                    "http://api.cybertaxonomy.org/cyprus/");
+            if (!results.get(0).equals("http://api.cybertaxonomy.org/cyprus/")) {
+                throw new RuntimeException("unexpected result item 0: " + results.get(0));
+            }
+            if (!results.get(1).equals("success")) {
+                throw new RuntimeException("unexpected result item 1: " + results.get(0));
+            }
+            // testing remote execution via ssh
+            dex.sshHost = "edit-int";
+            dex.setDrupalRoot(new File("/var/www/drupal-7"));
+            dex.setSiteURI(new URI("http://int.e-taxonomy.eu/dataportal/integration/cyprus"));
+            results = dex.execute(variableGet, "cdm_webservice_url");
+            if (!results.get(0).equals("http://int.e-taxonomy.eu/cdmserver/integration_cyprus/")) {
+                throw new RuntimeException("unexpected result item 0: " + results.get(0));
+            }
+
         } catch (IOException | InterruptedException | AssertionError e) {
             e.printStackTrace();
         }
+    }
+
+    public URI getSiteURI() {
+        return siteURI;
+    }
+
+    public void setSiteURI(URI siteURI) {
+        this.siteURI = siteURI;
+    }
+
+    public File getDrupalRoot() {
+        return drupalRoot;
+    }
+
+    public void setDrupalRoot(File drupalRoot) {
+        this.drupalRoot = drupalRoot;
+    }
+
+    public String getSshUser() {
+        return sshUser;
+    }
+
+    public void setSshUser(String sshUser) {
+        this.sshUser = sshUser;
+    }
+
+    public String getSshHost() {
+        return sshHost;
+    }
+
+    public void setSshHost(String sshHost) {
+        this.sshHost = sshHost;
     }
 }
