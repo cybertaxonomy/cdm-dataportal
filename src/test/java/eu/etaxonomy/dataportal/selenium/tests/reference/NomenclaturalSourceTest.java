4a70bee8e6019f8617f058ab5854c4f59c055629
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2020-12-03 12:01:47 +0100

    ref #8851 adapting test assertions to modified test data

--- src/test/java/eu/etaxonomy/dataportal/selenium/tests/reference/NomenclaturalSourceTest.java
+++ src/test/java/eu/etaxonomy/dataportal/selenium/tests/reference/NomenclaturalSourceTest.java
@@ -49,7 +49,7 @@ public class NomenclaturalSourceTest extends CdmDataPortalTestBase{
     public void testNomenclaturalReference() {
 
         WebElement acceptedName = p.getAcceptedName();
-        assertEquals("Nepenthes gracilis Korth., Verh. Nat. Gesch. Ned. Bezitt., Bot. 19: 22, t. 1 & 4. 1840 [& (J. T. K.) Spock & Scotty in Bot. Jahrb. Sol. 211: 591. 2194], [some typification note].", acceptedName.getText());
+        assertEquals("Nepenthes gracilis Korth., Verh. Nat. Gesch. Ned. Bezitt., Bot. 19: 22, t. 1 & 4. 1840 [& (J. T. K.) Spock & Scotty in Bot. Jahrb. Sol. 211: 591. 2194]1,2, [some typification note].", acceptedName.getText());
         WebElement referenceElement = acceptedName.findElement(By.cssSelector(".reference"));
         assertEquals("Verh. Nat. Gesch. Ned. Bezitt., Bot. 19: 22, t. 1 & 4. 1840", referenceElement.getText());
     }
@@ -59,8 +59,10 @@ public class NomenclaturalSourceTest extends CdmDataPortalTestBase{
 
         WebElement acceptedName = p.getAcceptedName();
         BaseElement protologueElement = new BaseElement(acceptedName.findElement(By.cssSelector(".protologue")));
-        assertEquals(1, protologueElement.getLinksInElement().size());
-        assertEquals("http://media.test/protologue/1234", protologueElement.getLinksInElement().get(0).getUrl());
+        assertEquals(3, protologueElement.getLinksInElement().size());
+        assertTrue(protologueElement.getLinksInElement().get(0).getUrl().endsWith("cdm_dataportal/taxon/5d9af5a8-c8ad-45e8-85df-ce6a01011fb9/synonymy#footnote-1"));
+        assertTrue(protologueElement.getLinksInElement().get(1).getUrl().endsWith("cdm_dataportal/taxon/5d9af5a8-c8ad-45e8-85df-ce6a01011fb9/synonymy#footnote-2"));
+        assertEquals("http://media.test/protologue/1234", protologueElement.getLinksInElement().get(2).getUrl());
     }
 
     @Test
