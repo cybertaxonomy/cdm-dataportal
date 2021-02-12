d3ba2bcdf10a37d04bde0c43e40a3b5d7cbe8530
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2021-02-12 09:06:01 +0100

    ref #9463 improving documentation

--- modules/cdm_dataportal/js/universalviewer/update-ak.sh
+++ modules/cdm_dataportal/js/universalviewer/update-ak.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf ~/workspaces/cdm/cdm-dataportal/modules/cdm_dataportal/js/universalviewer/uv
 cp -a ~/workspaces/libs/universalviewer/dist ~/workspaces/cdm/cdm-dataportal/modules/cdm_dataportal/js/universalviewer/uv
-
+rm ~/workspaces/cdm/cdm-dataportal/modules/cdm_dataportal/js/universalviewer/uv/uv.zip
