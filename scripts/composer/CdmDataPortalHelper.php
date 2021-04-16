57db05c431a9e919890c63e8c341e8197d2e18ad
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2021-04-16 16:59:41 +0200

    ref #9582 removing obsolete function from composer helper

--- scripts/composer/CdmDataPortalHelper.php
+++ scripts/composer/CdmDataPortalHelper.php
@@ -28,17 +28,4 @@ class CdmDataPortalHelper {
     $fs->symlink('./sites/all/themes/contrib/zen_dataportal/polyfills', $drupalRoot . '/polyfills');
     $event->getIO()->write("Created a symlink from ./polyfills to ./sites/all/themes/contrib/zen_dataportal/polyfills");
   }
-
-  public static function createInstallationPackage(Event $event) {
-    $fs = new Filesystem();
-    $drupalFinder = new DrupalFinder();
-    $drupalFinder->locateRoot(getcwd());
-    $drupalRoot = $drupalFinder->getDrupalRoot();
-
-    $parent = dirname($drupalRoot);
-    $pd = new PharData('drupal_7_cdm_dataportal.tar');
-    $pd->buildFromDirectory($parent, '#^'.preg_quote("$parent/parent-folder/", "#").'#');
-    $pd->compress(Phar::GZ);
-  }
-
 }
