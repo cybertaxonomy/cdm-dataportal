6ec1762a0f69b9edd2ad86c43e83cf2b84a8ff81
Katja Luther <K.Luther@bgbm.org>
2022-03-22 13:37:43 +0100

    ref #9967: add test for not designated

--- src/test/java/eu/etaxonomy/dataportal/selenium/tests/reference/NamePageTest.java
+++ src/test/java/eu/etaxonomy/dataportal/selenium/tests/reference/NamePageTest.java
@@ -70,9 +70,9 @@ public class NamePageTest extends CdmDataPortalTestBase{
         WebElement typeDesignationsContainer = p.getDataPortalContent().getElement().findElement(By.cssSelector("div.homotypic-synonymy-group"));
         List<WebElement> typeDesignations = typeDesignationsContainer.findElements(By.xpath("./div"));
         assertEquals(1, typeDesignations.size());
-        typeDesignations.get(0);
+
         assertTrue(typeDesignations.get(0).getAttribute("class").contains("cdm:SpecimenTypeDesignation uuid:c8738a91-daaf-42b3-8df6-b5641fff253d"));
-        assertTrue(typeDesignations.get(0).getElement().findElement(By.className("type-status")).contains("Type</span>: not designated"));
+        assertTrue(typeDesignations.get(0).findElement(By.className("type-status")).contains("Type</span>: not designated"));
 
     }
 
