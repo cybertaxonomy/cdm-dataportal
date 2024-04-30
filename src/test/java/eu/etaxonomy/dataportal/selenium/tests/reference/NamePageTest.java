7b36981d915b29b3271a02bd81c247bb37b29aa4
Katja <k.luther@bgbm.org>
2024-04-30 12:51:58 +0200

    ref #10322: adapt test

--- src/test/java/eu/etaxonomy/dataportal/selenium/tests/reference/NamePageTest.java
+++ src/test/java/eu/etaxonomy/dataportal/selenium/tests/reference/NamePageTest.java
@@ -67,7 +67,7 @@ public class NamePageTest extends CdmDataPortalTestBase{
     public void testNotDesignatedTypeDesignation() throws MalformedURLException {
 
         TaxonSynonymyPage p = new TaxonSynonymyPage(driver, getContext(), taxon_nodosilinea_sensenia_uuid);
-        WebElement typeDesignations = p.getNewHomotypicalGroupTypeDesignations();
+        WebElement typeDesignationsContainer = p.getNewHomotypicalGroupTypeDesignations();
         List<WebElement> typeDesignations = typeDesignationsContainer.findElements(By.xpath("./div"));
         //GenericPortalPage p = new GenericPortalPage(driver, getContext(), "taxon/" + name_nodosilinea_sensenia_uuid.toString() + "/synonymy");
         // expecting to land on name page, see NamePageRedirectTest for other cases
