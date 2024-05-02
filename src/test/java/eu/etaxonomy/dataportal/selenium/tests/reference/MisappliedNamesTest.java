7a79fe6d10e869a3d3e5533aa40a45a4f1b4d63e
Katja <k.luther@bgbm.org>
2024-05-02 12:12:26 +0200

    ref #10322: test output

--- src/test/java/eu/etaxonomy/dataportal/selenium/tests/reference/MisappliedNamesTest.java
+++ src/test/java/eu/etaxonomy/dataportal/selenium/tests/reference/MisappliedNamesTest.java
@@ -15,8 +15,13 @@ import java.util.UUID;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 
 import eu.etaxonomy.cdm.common.UTF8;
 import eu.etaxonomy.dataportal.DataPortalSite;
