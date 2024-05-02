4b4c87f5d9050f7a68145d2574871913698e748f
Katja <k.luther@bgbm.org>
2024-05-02 11:05:58 +0200

    ref #10322: adadapt type test

--- src/test/java/eu/etaxonomy/dataportal/selenium/tests/reference/MisappliedNamesTest.java
+++ src/test/java/eu/etaxonomy/dataportal/selenium/tests/reference/MisappliedNamesTest.java
@@ -75,9 +75,9 @@ public class MisappliedNamesTest extends CdmDataPortalTestBase{
     public void tesIssue5647() throws MalformedURLException {
 
         TaxonSynonymyPage p = new TaxonSynonymyPage(driver, getContext(), miconia_cubacinerea_Uuid);
-        logger.debug("misapplied name 1 text" + p.getMisappliedName(1).getText()));
-        logger.debug("misapplied name 2 text" + p.getMisappliedName(2).getText()));
-        logger.debug("misapplied name 3 text" + p.getMisappliedName(3).getText()));
+        logger.debug("misapplied name 1 text" + p.getMisappliedName(1).getText());
+        logger.debug("misapplied name 2 text" + p.getMisappliedName(2).getText());
+        logger.debug("misapplied name 3 text" + p.getMisappliedName(3).getText());
         assertEquals("p.p. MAN for Ossaea glomerata sec. A&S1; sec. Species solaris", p.getMisappliedName(1).getText());
 
         assertEquals("partial MAN for Ossaea glomerata sec. A&S1: 33; auct. sec. A&S1", p.getMisappliedName(2).getText());
