718735b5f8521e4b45f2111a447175523c70521e
Katja <k.luther@bgbm.org>
2025-02-26 11:21:40 +0100

    adapt test

--- src/test/java/eu/etaxonomy/dataportal/selenium/tests/reference/SpecimensTopDownViewTest.java
+++ src/test/java/eu/etaxonomy/dataportal/selenium/tests/reference/SpecimensTopDownViewTest.java
@@ -69,7 +69,7 @@ public class SpecimensTopDownViewTest extends CdmDataPortalTestBase {
         assertEquals("2017E68", dl1.joinedDescriptionElementText("Most significant identifier:"));
         assertEquals("2017E68", dl1.joinedDescriptionElementText("Accession number:"));
         assertEquals("destroyed", dl1.joinedDescriptionElementText("Status:"));
-        assertEquals("Epitype (designated by Kretschmann, J., Žerdoner ?alasan, A. & Kusber, W.-H. 20172)",
+        assertEquals("Epitype (designated by Kretschmann & al. 20172)",
                 dl1.joinedDescriptionElementText("Specimen type designations:"));
         // 2
         assertEquals("gathering in-situ from:", contentElements.get(1).getText());
